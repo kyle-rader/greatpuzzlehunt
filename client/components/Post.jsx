7e160a53f64d2485c9ceeaba5955b235ce96c601
Kyle W. Rader <kyle@kylerader.ninja>
2016-02-23 12:32:24 -0800

    Adding date lables to posts.

--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -5,7 +5,38 @@ Post = React.createClass({
         post: React.PropTypes.object.isRequired,
     },
 
+    getDateLabel() {
+        let timeUnit = null;
+        let plural = '';
+        let timeValue = 0;
+
+        let seconds = Math.floor((new Date() - this.props.post.createdAt) / 1000);
+        let interval = Math.floor(seconds / 31536000);
+
+        if (interval > 1) {
+            return interval + ' years ago';
+        }
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+            return interval + ' months ago';
+        }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+            return interval + ' days ago';
+        }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+            return interval + ' hours ago';
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+            return interval + ' minutes ago';
+        }
+        return Math.floor(seconds) + ' seconds ago';
+    },
+
     render() {
+
         return (
         <div className="event">
             <div className="content">
@@ -15,7 +46,7 @@ Post = React.createClass({
                     </a>
                     posted
                     <div className="date">
-                        5m ago
+                        {this.getDateLabel()}
                     </div>
                 </div>
                 <div className="extra text">
@@ -23,7 +54,7 @@ Post = React.createClass({
                 </div>
                 <div className="meta">
                     <a className="like">
-                        <i className="like icon"></i> x likes
+                        <i className="like icon"></i> {this.props.post.likes} likes
                     </a>
                 </div>
             </div>
