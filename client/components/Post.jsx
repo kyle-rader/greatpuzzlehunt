6c7fa445ae154c0ece552a11cbccd926ac43e618
Kyle W. Rader <kyle@kylerader.ninja>
2016-02-23 01:10:50 -0800

    Adding Feed with new post form at top.

--- client/components/Post.jsx
+++ client/components/Post.jsx
@@ -6,6 +6,28 @@ Post = React.createClass({
     },
 
     render() {
-        return <li>{this.props.post.text}</li>;
+        return (
+        <div className="event">
+            <div className="content">
+                <div className="summary">
+                    <a className="user">
+                        UserName 
+                    </a>
+                    posted
+                    <div className="date">
+                        5m ago
+                    </div>
+                </div>
+                <div className="extra text">
+                    {this.props.post.text}
+                </div>
+                <div className="meta">
+                    <a className="like">
+                        <i className="like icon"></i> x likes
+                    </a>
+                </div>
+            </div>
+        </div>
+        );
     }
 });
