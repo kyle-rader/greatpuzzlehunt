fa3013b41b98e72011d72e051bcbe8de900348e3
Kyle W. Rader <kyle@kylerader.ninja>
2016-03-03 04:12:17 -0800

    Adding HomePanel react component and JS and CSS.

---
+++ client/components/public/HomePanel.jsx
@@ -0,0 +1,12 @@
+HomePanel = React.createClass({
+    render() {
+        return (
+        <div className="text-center column home-panel">
+            <a className="icon-btn" href={this.props.link}>
+                <i className={this.props.icon}></i>
+                <h2>{this.props.title}</h2>
+            </a>
+            {this.props.extraText}
+        </div>);
+    }
+});
