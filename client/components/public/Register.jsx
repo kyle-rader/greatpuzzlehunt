9aa250e057edc31df69dc2c4725f70cbbc5af211
Kyle W. Rader <kyle@kylerader.ninja>
2016-03-05 19:55:30 -0800

    Implementing Registration method.

--- client/components/public/Register.jsx
+++ client/components/public/Register.jsx
@@ -5,8 +5,7 @@ Register = React.createClass({
     getInitialState() {
       return {
         err: null,
-        username: '',
-        password: ''
+        newAccountEmail: null
       };
     },
 
@@ -15,7 +14,7 @@ Register = React.createClass({
       // init dropdowns
       $('#registerForm .ui.dropdown').dropdown();
 
-      // initi form validation
+      // Init form validation and submission
       $(form).form({
         fields: {
           firstname: {
@@ -70,105 +69,145 @@ Register = React.createClass({
           }
         },
         inline: true,
+        onSuccess: (event, fields) => {
+          event.preventDefault();
+          
+          // Call Meteor method to create account.
+          Meteor.call('userCreate', fields, (err, result) => {
+            if(err) {
+              this.setState({err: err});
+            } else {
+              this.setState({err: null, newAccountEmail: result.email});
+              console.log(result);
+            }
+
+          });
+        }
       });
     },
 
-    createAccount(event) {
-        event.preventDefault();
+    getErrorMsg() {
+      if (this.state.err) {
+        return (
+        <div className="ui error message">
+          {this.state.err.reason}
+        </div>);
+      }
+    },
 
-        let fields = $(this.refs.registerForm).form('get values firstname lastname username password acceptTOC');
-        
-        let validation = this.validateForm(fields);
+    getSuccessMsg() {
+      if (this.state.newAccountEmail) {
+        return (
+          <div className="ui success message">
+            <h3>Welcome to the WWU Puzzle Hunt!</h3>
+            <h4>A verification email has been sent to <strong>{this.state.newAccountEmail}</strong></h4>
+            <a className="ui facebook button" target="_blank" href="https://www.facebook.com/events/160687404314460/">
+              <i className="facebook icon"></i>
+              Share
+            </a>
+          </div>);
+      }
+    },
 
-        if (validation.hasError) {
-          this.setState({err: validation});
-        }
-        else {
-          Accounts.createUser({
-            username: fields.username,
-            password: fields.password,
-            profile: {
-              firstname: fields.firstname,
-              lastname: fields.lastname,
-              acceptTOC: fields.acceptTOC
-            }
-          }, (err) => {
-            if (err) {
-              console.log(err);
-              this.setState({err: err.reason});
-            }
-          });
-        }
+    getForm() {
+      if (!this.state.newAccountEmail) {
+        return (
+          <form id="registerForm" className="ui form" ref="registerForm">
+            <div className="ui raised segment transparent-bg">
+              <h2 className="ui blue header">
+                <div className="content">
+                  On your marks! Get set! Puzzles!
+                </div>
+              </h2>
+              <div className="two fields">
+                <div className="field">
+                  <input type="text" name="firstname" placeholder="First Name" autoComplete="off" defaultValue={this.state.firstname}/>
+                </div>
+                <div className="field">
+                  <input type="text" name="lastname" placeholder="Last Name" autoComplete="off" defaultValue={this.state.lastname}/>
+                </div>
+              </div>
+              <div className="two fields">
+                <div className="field">
+                  <div className="ui left icon input">
+                    <i className="user icon"></i>
+                    <input type="text" name="username" placeholder="WWU Username" autoComplete="off" defaultValue={this.state.username}/>
+                  </div>
+                </div>
+                <div className="field">
+                  <select name="userType" className="ui fluid dropdown">
+                    <option value="student">Student</option>
+                    <option value="faculty">Faculty/ Staff</option>
+                  </select>
+                </div>
+              </div>
+              <div className="two fields">
+                <div className="field">
+                  <div className="ui left icon input">
+                    <i className="lock icon"></i>
+                    <input type="password" name="password" placeholder="Password" autoComplete="off" defaultValue={this.state.password}/>
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="ui left icon input">
+                    <i className="lock icon"></i>
+                    <input type="password" name="confirmPassword" placeholder="Confirm Password" autoComplete="off" defaultValue={this.state.confirmPassword}/>
+                  </div>
+                </div>
+              </div>
+              <div className="field">
+                <input className="ui input" name="phone" placeholder="Phone Number" defaultValue={this.state.phone} />
+              </div>
+
+              <div className="field">
+                <input className="ui input" name="major" placeholder="Major / Department / Field of Interest" defaultValue={this.state.major}/>
+              </div>
+
+              <div className="field">
+                <label>Want an awesome T-Shirt?</label>
+                <a target="_blank" href="http://commerce.cashnet.com/GreatPuzzleHunt2016" className="ui wwu-btn fluid right labeled icon button">
+                  <i className="shop icon"></i>
+                  Order your Puzzle Master T-Shirt!
+                </a>
+              </div>
+
+              <input className="ui fluid large blue submit button" type="submit" value="Join" />
+            </div>
+          </form>
+        );
+      }
+    },
+
+    setuptest() {
+      let form = $(this.refs.registerForm);
+
+      form.form('set values', {
+        firstname: 'Kyle',
+        lastname: 'Rader',
+        username: 'kyle+raderk',
+        password: 'testing',
+        confirmPassword: 'testing',
+        phone: '1231231234',
+        major: 'computer science'
+      });
     },
 
     render() {
+
       return (
       <div className="register ui middle aligned center aligned grid custom-bg map">
           <div className="column">
-              <form id="registerForm" className="ui form" ref="registerForm">
-                <div className="ui raised segment transparent-bg">
-                  <h2 className="ui blue header">
-                    <div className="content">
-                      On your marks! Get set! Puzzles!
-                    </div>
-                  </h2>
-                  <div className="two fields">
-                    <div className="field">
-                      <input type="text" name="firstname" placeholder="First Name" autoComplete="off" defaultValue={this.state.firstname}/>
-                    </div>
-                    <div className="field">
-                      <input type="text" name="lastname" placeholder="Last Name" autoComplete="off" defaultValue={this.state.lastname}/>
-                    </div>
-                  </div>
-                  <div className="two fields">
-                    <div className="field">
-                      <div className="ui left icon input">
-                        <i className="user icon"></i>
-                        <input type="text" name="username" placeholder="WWU Username" autoComplete="off" defaultValue={this.state.username}/>
-                      </div>
-                    </div>
-                    <div className="field">
-                      <select name="userType" className="ui fluid dropdown">
-                        <option value="student">Student</option>
-                        <option value="faculty">Faculty/ Staff</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="two fields">
-                    <div className="field">
-                      <div className="ui left icon input">
-                        <i className="lock icon"></i>
-                        <input type="password" name="password" placeholder="Password" autoComplete="off" defaultValue={this.state.password}/>
-                      </div>
-                    </div>
-                    <div className="field">
-                      <div className="ui left icon input">
-                        <i className="lock icon"></i>
-                        <input type="password" name="confirmPassword" placeholder="Confirm Password" autoComplete="off" defaultValue={this.state.confirmPassword}/>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="field">
-                    <input className="ui input" name="phone" placeholder="Phone Number" defaultValue={this.state.phone} />
-                  </div>
-                  <div className="field">
-                    <input className="ui input" name="major" placeholder="Major / Department / Field of Interest" defaultValue={this.state.major}/>
-                  </div>
 
-                  <div className="field">
-                    <label>Want an awesome T-Shirt?</label>
-                    <a target="_blank" href="http://commerce.cashnet.com/GreatPuzzleHunt2016" className="ui fluid right labeled icon button">
-                      <i className="shop icon"></i>
-                      Order your Puzzle Master T-Shirt!
-                    </a>
-                  </div>
-
-                  <input className="ui fluid large blue submit button" type="submit" value="Join" />
-                </div>
-              </form>
+              {this.getForm()}
+              {this.getSuccessMsg()}
+              {this.getErrorMsg()}
 
               <div className="ui message transparent-bg">
                 <a href="/login">Log In</a>
+              </div>
+
+              <div className="ui message transparent-bg">
+                <a className="ui button" onClick={this.setuptest}>Test</a>
               </div>
           </div>
       </div>);
