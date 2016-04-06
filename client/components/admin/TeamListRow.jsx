71740d57b4d1d7c80452c9518dc329fdfffb1ab4
Kyle W. Rader <kyle@kylerader.ninja>
2016-04-06 16:43:37 -0700

    Get initial Teams list up and running.

--- client/components/admin/TeamListRow.jsx
+++ client/components/admin/TeamListRow.jsx
@@ -1,51 +1,40 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 
-export default class TeamListRow extends React.Component {
+TeamListRow = React.createClass({
 
-    constructor(props) {
-        super(props);
-        this.state = {
+    propTypes: {
+        loading: React.PropTypes.bool,
+        team: React.PropTypes.object,
+        members: React.PropTypes.array
+    },
+
+    getInitialState() {
+        return {
             editMode: false
         };
-    }
+    },
+
+    mixins:[ReactMeteorData],
+    getMeteorData() {
+        let data = {};
+        let membersHandle = Meteor.subscribe('team.members', this.props.team._id);
+
+        if (membersHandle.ready()) {
+            data.members = Meteor.users.find({"profile.teamId": this.props.team._id}).fetch();
+        }
+        return data;
+    },
 
     enableEdit(user) {
         this.setState({editMode: true});
-    }
+    },
 
     saveTeam() {
-        // let name = this.refs.firstname.value;
-        // let password = this.refs.lastname.value;
-
-        // let editBtn = $(this.refs.editBtn);
-
-        // Meteor.call('userAdminUpdate', {
-        //     _id: this.props.user._id,
-        //     firstname: firstname,
-        //     lastname: lastname,
-        //     username: username,
-        //     email: email
-        // }, (err, result) => {
-        //     if (err) {
-        //         console.log(err);
-        //         editBtn.attr('data-content', 'Failed to save user! 😰');
-        //     } else {
-        //         editBtn.attr('data-content', `${firstname} saved! 😀`);
-        //     }
-
-        //     editBtn.popup({
-        //         on: 'manual'
-        //     }).popup('show');
-        //     setTimeout(() => {
-        //         editBtn.popup('hide');
-        //     }, 3000);
-        // });
-        // this.setState({editMode: false});
-    }
+    },
 
     deleteTeam(event) {
-    }
+    },
 
     getName() {
         if (this.state.editMode) {
@@ -57,10 +46,10 @@ export default class TeamListRow extends React.Component {
         } else {
             return (
             <td>
-                {this.props.team}
+                {this.props.team.name}
             </td>);
         }
-    }
+    },
 
     getPassword() {
         if (this.state.editMode) {
@@ -76,60 +65,57 @@ export default class TeamListRow extends React.Component {
                 </div>
             </td>);
         }
-    }
+    },
+
+    getMembers() {
+        if (!this.data.members) return null;
+
+        return this.data.members.map((member) => {
+            return <li key={member._id}><strong>{member.profile.displayname}</strong> | {member.emails[0].address} | {member.profile.phone}</li>
+        });
+    },
 
     getEditButton() {
         if (this.state.editMode) {
-            return (<div ref="editBtn" className="ui green basic button" title="Edit User" onClick={this.saveTeam.bind(this)}><i className="save icon"></i></div>);
+            return (<div ref="editBtn" className="ui green basic button" title="Edit User" onClick={this.saveTeam}><i className="save icon"></i></div>);
         } else {
-            return (<div ref="editBtn" className="ui green basic button" title="Save User" onClick={this.enableEdit.bind(this)}><i className="pencil icon"></i></div>);
+            return (<div ref="editBtn" className="ui green basic button" title="Save User" onClick={this.enableEdit}><i className="pencil icon"></i></div>);
         }
-    }
+    },
 
     componentDidMount() {
         //console.log(`Team mounted`, this.props);
-    }
+    },
 
     componentWillReceiveProps() {
         this.setState({editMode: false});
-    }
+    },
 
     render() {
 
-        if (this.props.loading) {
-            return (
-                <tr>
-                    <td colSpan="4">Loading ...</td>
-                </tr>
-            );
-        }
-
-        console.log(this.props);
-
         return (
         <tr>
-            <td></td>
+            {this.getName()}
 
-            <td></td>
+            {this.getPassword()}
 
-            <td></td>
-            
+            <td>
+                <div className="ui list">
+                    { this.getMembers() }
+                </div>
+            </td>
+            {/*
             <td>
                 <div className="ui three icon tiny compact buttons">
                     {this.getEditButton()}
-                    <div className="ui disabled red basic button" title="Delete User" onClick={this.deleteTeam.bind(this)}>
+                    <div className="ui disabled red basic button" title="Delete User" onClick={this.deleteTeam}>
                         <i className="trash icon"></i>
                     </div>
                 </div>
             </td>
+            */}
         </tr>
-        
         );
     }
-}
+});
 
-TeamListRow.propTypes = {
-    loading: React.PropTypes.bool,
-    team: React.PropTypes.object,
-    members: React.PropTypes.array
-};
