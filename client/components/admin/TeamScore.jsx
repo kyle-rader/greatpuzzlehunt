676918e824b968a43ce69358ed31e3ed9f325d6a
Kyle W. Rader <kyle@kylerader.ninja>
2016-04-11 17:54:45 +0800

    Adding score sum.

--- client/components/admin/TeamScore.jsx
+++ client/components/admin/TeamScore.jsx
@@ -30,12 +30,20 @@ export default TeamScore = React.createClass({
             return <div className="ui message">Loading</div>;
         }
 
+        let total = 0;
+        for(let i = 0; i < this.data.attempts.length; i++) {
+            total += (((this.data.attempts[i].finalScore + (this.data.attempts[i].puzzleId.toString() === 'ri4BZ8enkd5x8NgP5' ? 900000 : 0)) / 1000));
+        }
+
         return this.data.attempts.map((attempt) => {
+
+
             if (!attempt.finishTime) {
                 return <div className="ui small warning message" key={attempt._id}>Still solving</div>;
             }
             return (
             <div className="ui info message" key={attempt._id}>
+                <strong>Team Sum:</strong> &nbsp; {total.toFixed(2)}
                 <div className="ui items">
                     <div className="item">
                         Start:&nbsp; {attempt.startTime.toTimeString()}
