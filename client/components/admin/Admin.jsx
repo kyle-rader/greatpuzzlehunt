977af7cb3d520b77585a0263186e36f80af957b2
Kyle Rader <kyle@kylerader.ninja>
2016-08-30 23:59:30 -0700

    Wrap Admin page in AuthedComponentContainer

--- client/components/admin/Admin.jsx
+++ client/components/admin/Admin.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import UserList from './UserList.jsx';
@@ -8,84 +8,62 @@ import BulkEmail from './BulkEmail.jsx';
 import GamePlay from './GamePlay.jsx';
 import Scoreing from './Scoring.jsx';
 
-Admin = React.createClass({
+Admin = class Admin extends Component {
 
-    getInitialState() {
-        return {
-            pageComp: Scoring //UserList
-        }
-    },
-
-    mixins: [ReactMeteorData],
-    getMeteorData() {
-        return {
-            user: Meteor.user()
-        };
-    },
-
-    componentDidMount() {
-        $(this.refs.tabMenu).find('.item').tab();
-    },
-
-    setPage(targetPage) {
-        this.setState({
-            pageComp: targetPage
-        });
-    },
-
-    render() {
-        // First Check Access
-        if (!this.data.user) {
-            return <LoadingSegment />
-        }
-        else if (this.data.user.roles.indexOf('admin') < 0) {
-            return <Login />
-        }
-
-        let pageComp = this.state.pageComp ? <this.state.pageComp /> : <div className="basic segment">Oops, no page found</div>;
-
-        return (
-            <div className="custom-bg red-square">
-                <br/>
-                <div className="ui raised segment transparent-bg">
-                    <h3 className="ui violet center aligned header">Admin Panel</h3>
-                    <div className="ui labeled icon menu">
-                        <a className="item" onClick={this.setPage.bind(this, UserList)}>
-                            <i className="green user icon"></i>
-                            Users
-                        </a>
-                        <a className="item" onClick={this.setPage.bind(this, TeamList)}>
-                            <i className="blue users icon"></i>
-                            Teams
-                        </a>
-                        <a className="item" onClick={this.setPage.bind(this, PuzzleDashboard)}>
-                            <i className="violet puzzle icon"></i>
-                            Puzzles
-                        </a>
-                        <a className="item" onClick={this.setPage.bind(this, BulkEmail)}>
-                            <i className="orange mail icon"></i>
-                            Email
-                        </a>
-                        <a className="item" onClick={this.setPage.bind(this, GamePlay)}>
-                            <i className="red gamepad icon"></i>
-                            The Game
-                        </a>
-                        <a className="item" onClick={this.setPage.bind(this, Scoring)}>
-                            <i className="yellow trophy icon"></i>
-                            Scoring
-                        </a>
-                    </div>
-                    {pageComp}
-                </div>
-            </div>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageComp: Scoring //UserList
     }
-});
+  }
 
-/*
-Admin.propTypes = {
-    list: React.PropTypes.object,
-    ...
+  componentDidMount() {
+      $(this.refs.tabMenu).find('.item').tab();
+  }
+
+  setPage(targetPage) {
+      this.setState({
+          pageComp: targetPage
+      });
+  }
+
+  render() {
+
+    let pageComp = this.state.pageComp ? <this.state.pageComp /> : <div className="basic segment">Oops, no page found</div>;
+
+    return (
+      <AuthedComponentContainer params={{accessLevel: 'admin'}}>
+        <div className="ui container">
+          <PuzzlePageTitle title="Admin" />
+          <div className="ui labeled icon menu">
+            <a className="item" onClick={this.setPage.bind(this, UserList)}>
+              <i className="green user icon"></i>
+              Users
+            </a>
+            <a className="item" onClick={this.setPage.bind(this, TeamList)}>
+              <i className="blue users icon"></i>
+              Teams
+            </a>
+            <a className="item" onClick={this.setPage.bind(this, PuzzleDashboard)}>
+              <i className="violet puzzle icon"></i>
+              Puzzles
+            </a>
+            <a className="item" onClick={this.setPage.bind(this, BulkEmail)}>
+              <i className="orange mail icon"></i>
+              Email
+            </a>
+            <a className="item" onClick={this.setPage.bind(this, GamePlay)}>
+              <i className="red gamepad icon"></i>
+              The Game
+            </a>
+            <a className="item" onClick={this.setPage.bind(this, Scoring)}>
+              <i className="yellow trophy icon"></i>
+              Scoring
+            </a>
+          </div>
+          {pageComp}
+        </div>
+      </AuthedComponentContainer>
+    );
+  }
 }
-        
-*/
