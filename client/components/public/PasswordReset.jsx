2bbbab1f25c91a83ec684e64a7a191643f2b4c49
Kyle Rader <kyle@kylerader.ninja>
2016-08-30 23:49:00 -0700

    Update login, forgot password, and reset password pages

--- client/components/public/PasswordReset.jsx
+++ client/components/public/PasswordReset.jsx
@@ -1,112 +1,110 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 
 // Reset Password comp
 
-PasswordReset = React.createClass({
+PasswordReset = class PasswordReset extends Component {
 
-    propTypes: {
-        token: React.PropTypes.string.isRequired
-    },
+  constructor(props) {
+    super(props);
+    this.state = {
+      err: null
+    };
+  }
 
-    getInitialState() {
-      return {
-        err: null
-      };
-    },
+  componentDidMount() {
+    let form = $(this.refs.resetForm);
 
-    componentDidMount() {
-      let form = $(this.refs.resetForm);
-
-      // Init form validation and submission
-      $(form).form({
-        fields: {
-          password: {
-            identifier: 'password',
-            rules: [{
-              type: 'empty',
-              prompt: 'Enter a password'
-            },
-            {
-              type: 'minLength[6]',
-              prompt: 'Min length of 6!'
-            }]
+    // Init form validation and submission
+    $(form).form({
+      fields: {
+        password: {
+          identifier: 'password',
+          rules: [{
+            type: 'empty',
+            prompt: 'Enter a password'
           },
-          confirmPassword: {
-            identifier: 'confirmPassword',
-            rules: [{
-              type: 'match[password]',
-              prompt: 'Passwords do not match!'
-            }]
-          }
+          {
+            type: 'minLength[6]',
+            prompt: 'Min length of 6!'
+          }]
         },
-        inline: true,
-        onSuccess: (event, fields) => {
-          event.preventDefault();
-
-          // Reset Password
-          Accounts.resetPassword(this.props.token, fields.password, (err, result) => {
-            if (err) {
-              this.setState({err: err});
-            } else {
-              FlowRouter.go('/team');
-            }
-          });
+        confirmPassword: {
+          identifier: 'confirmPassword',
+          rules: [{
+            type: 'match[password]',
+            prompt: 'Passwords do not match!'
+          }]
         }
-      });
-    },
+      },
+      inline: true,
+      onSuccess: (event, fields) => {
+        event.preventDefault();
 
-    getErrorMsg() {
-      if (this.state.err) {
-        return (
-        <div className="ui error message">
-          {this.state.err.reason}
-        </div>);
+        // Reset Password
+        Accounts.resetPassword(this.props.token, fields.password, (err, result) => {
+          if (err) {
+            this.setState({err: err});
+          } else {
+            FlowRouter.go('/team');
+          }
+        });
       }
-    },
+    });
+  }
 
-    getForm() {
-        return (
-        <form id="resetForm" className="ui huge form" ref="resetForm">
-            <div className="ui raised segment transparent-bg">
-                <h2 className="ui orange header">
-                    <div className="content">
-                      Choose a New Password
-                    </div>
-                </h2>
-
-                <div className="field">
-                  <div className="ui left icon input">
-                    <i className="lock icon"></i>
-                    <input type="password" name="password" placeholder="Password" autoComplete="off" defaultValue={this.state.password}/>
-                  </div>
-                </div>
-                <div className="field">
-                  <div className="ui left icon input">
-                    <i className="lock icon"></i>
-                    <input type="password" name="confirmPassword" placeholder="Confirm Password" autoComplete="off" defaultValue={this.state.confirmPassword}/>
-                  </div>
-                </div>
-
-                <input className="ui fluid large orange submit button" type="submit" value="Reset" />
-            </div>
-        </form>
-        );
-    },
-
-    render() {
-
+  getErrorMsg() {
+    if (this.state.err) {
       return (
-      <div className="password-reset ui middle aligned center aligned grid custom-bg red-square">
-          <div className="column">
-
-              {this.getForm()}
-              {this.getErrorMsg()}
-
-              <div className="ui message">
-                <a href="/login">Log In</a>
-              </div>
-          </div>
+      <div className="ui error message">
+        {this.state.err.reason}
       </div>);
     }
-});
+  }
+
+  getForm() {
+    return (
+    <form id="resetForm" className="ui huge form" ref="resetForm">
+      <div className="ui raised segment transparent-bg">
+        <h2 className="ui orange header">
+          <div className="content">
+            Choose a New Password
+          </div>
+        </h2>
+
+        <div className="field">
+          <div className="ui left icon input">
+            <i className="lock icon"></i>
+            <input type="password" name="password" placeholder="Password" autoComplete="off" defaultValue={this.state.password}/>
+          </div>
+        </div>
+        <div className="field">
+          <div className="ui left icon input">
+            <i className="lock icon"></i>
+            <input type="password" name="confirmPassword" placeholder="Confirm Password" autoComplete="off" defaultValue={this.state.confirmPassword}/>
+          </div>
+        </div>
+
+        <input className="ui fluid large orange submit button" type="submit" value="Reset" />
+      </div>
+    </form>
+    );
+  }
+
+  render() {
+
+    return (
+    <div className="password-reset ui middle aligned center aligned grid custom-bg red-square">
+        <div className="column">
+
+            {this.getForm()}
+            {this.getErrorMsg()}
+
+            <div className="ui message">
+              <a href="/login">Log In</a>
+            </div>
+        </div>
+    </div>
+    );
+  }
+}
