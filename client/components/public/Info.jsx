5ab3066da1786023f5005a89d4449d9b39f46976
Kyle Rader <kyle@kylerader.ninja>
2016-09-19 02:01:43 -0700

    Overhaul info page

--- client/components/public/Info.jsx
+++ client/components/public/Info.jsx
@@ -1,232 +1,189 @@
 import React, { Component } from 'react';
+import { renderToString } from 'react-dom/server';
+import { Accordion, List, Icon, Header, Segment, Button, Image } from 'stardust';
 
 Info = class Info extends Component {
+
+  componentDidMount() {
+    $('html, body').scrollTop(0);
+  }
 
   render() {
     return (
       <div className="ui container">
         <PuzzlePageTitle title="Information"/>
 
-        <div className="ui stackable grid">
-          <div className="two column row">
-            <div className="column">
-              <a className="ui blue fluid big button" href="/pdfs/The Great Puzzle Hunt Rules of Play and Tips.pdf" target="_blank">
-                Rules of Play and Hunting Tips
-              </a>
-            </div>
-            <div className="column">
-              <a className="ui blue fluid big button" href="/pdfs/puzzle-hunt-map.pdf" target="_blank">
-                Puzzle Hunt Campus Map
-              </a>
-            </div>
-          </div>
-        </div>
+        <Segment basic>
+          <h3>Useful downloads</h3>
+          <Button color="blue" animated="fade" as="a" href="/pdfs/The Great Puzzle Hunt Rules of Play and Tips.pdf" target="_blank">
+            <Button.Content visible>Rules of Play and Tips</Button.Content>
+            <Button.Content hidden>
+              <Icon name='download' />
+              Download
+            </Button.Content>
+          </Button>
+          <Button color="blue" animated="fade" as="a" href="/pdfs/puzzle-hunt-map.pdf" target="_blank">
+            <Button.Content visible>Puzzle Campus Map</Button.Content>
+            <Button.Content hidden>
+              <Icon name='download' />
+              Download
+            </Button.Content>
+          </Button>
+        </Segment>
 
-        {/* WHAT IS IT? */}
-        <h2 className="ui top attached header">
-          <i className="red help icon"></i>
-          <div className="content">
-            What is The WWU Great Puzzle Hunt?
-            <div className="sub header">(A lot of fun is what!)</div>
-          </div>
-        </h2>
-        <div className="ui attached segment">
-          A puzzle hunt is like a scavenger hunt, with puzzles.
-          Teams (4-6 is a nice size) travel on foot about campus (outdoors)
-          solving a collection of puzzles (logic puzzles, word scrambles, trivia, etc.).
-          Lots of prizes will be awarded. Whether your team places first or two hundred and fifty-first,
-          competing in a puzzle hunt is a great way to stretch your mental muscles, bond with your teammates,
-          and have a lot of fun!
-        </div>
+        <Accordion styled fluid>
 
-        {/* WHEN IS IT? */}
-        <h2 className="ui top attached header">
-          <i className="orange calendar icon"></i>
-          <div className="content">
+          <Accordion.Title>
+            <Icon color="red" size="huge" name="dropdown"/>
+            What is the Great Puzzle Hunt?
+          </Accordion.Title>
+          <Accordion.Content>
+            A puzzle hunt is like a scavenger hunt, with puzzles.
+            Teams (size 4-6) travel on foot about campus (outdoors) solving a
+            collection of puzzles (logic puzzles, word scrambles, trivia, etc.).
+            Lots of prizes will be awarded. Whether your team places first or two
+            hundred and fifty-first, competing in a puzzle hunt is a great way to
+            stretch your mental muscles, bond with your teammates, and have a lot of fun!
+          </Accordion.Content>
+
+          <Accordion.Title>
+            <Icon color="orange" size="big" name="dropdown"/>
             When is it?
-            <div className="sub header">Saturday, April 1st, 2017 @ 10:00 AM, Red Square</div>
-          </div>
-        </h2>
-        <div className="ui attached segment">
-          <strong>Important Dates</strong>
-          <div className="ui list">
-            <div className="item">
-              <div className="ui horizontal label">
-                TBD
-              </div>
-              Registration Opens
-            </div>
-            <div className="item">
-              <div className="ui horizontal label">
-                TBD
-              </div>
-              Early Bird Registration Closes
-            </div>
-            <div className="item">
-              <div className="ui horizontal label">
-                Saturday, April 1st, 2017 &nbsp;@ 10:00 AM
-              </div>
-              Game Day!
-            </div>
-          </div>
-        </div>
+          </Accordion.Title>
+          <Accordion.Content>
+            <h4>
+              April 1, 2017 (Saturday) at 10:00 AM, Red Square, WWU
+            </h4>
+            Other important dates:
+            <List className='bulleted'>
+              <List.Item>Now: <a target="_blank" href="https://actionsprout.io/7E111B">Pre-Register and get notified when registration opens!</a></List.Item>
+              <List.Item>March 10, 2017: Early Bird registration closes.</List.Item>
+              <List.Item>March 23, 2017: T-Shirt Pre-Order deadline (in order to pick up your shirt at check-in on April 1, 2017)</List.Item>
+              <List.Item>March 30, 2017: Registration Closes (Or earlier if team limit is reached).</List.Item>
+            </List>
+          </Accordion.Content>
 
-        {/* WHO IS IT FOR? */}
-        <h2 className="ui top attached header">
-          <i className="large icons">
-            <i className="yellow users icon"></i>
-            <i className="blue help corner icon"></i>
-          </i>
-          <div className="content">
-            Who is invited?
-            <div className="sub header">Students, faculty, staff, Alumni, and Community!</div>
-          </div>
-        </h2>
-        <div className="ui attached segment">
-          We will be adding an OPEN division in 2017. More information forthcoming.
-          <a href="/register"> Sign up</a> and create a team of up to 6 people, or
-          register as an individual and browse existing teams and find one in need of your skills!
-        </div>
+          <Accordion.Title>
+            <Icon color="yellow" size="big" name="dropdown"/>
+            Who is it for?
+          </Accordion.Title>
+          <Accordion.Content>
+            Students, Faculty, Staff, Alumni, Community, Everyone!<br/>
+            <small>* Children under 14 must be accompanied at all times by a parent/legal guardian who must also be registered on the same team as the child.</small>
+          </Accordion.Content>
 
-        {/* COST? */}
-        <h2 className="ui top attached header">
-          <i className="green money icon"></i>
-          <div className="content">
+          <Accordion.Title>
+            <Icon color="olive" size="big" name="dropdown"/>
+            What team divisions are there?
+          </Accordion.Title>
+          <Accordion.Content>
+            <List className='bulleted'>
+              <List.Item description="WWU Students - All team members must be currently enrolled at WWU (undergrad or grad)"/>
+              <List.Item description="High School - All team members must be currently enrolled in high school"/>
+              <List.Item description="WWU Alumni - 4 or more team members must be WWU Alumni"/>
+              <List.Item description="Non-WWU Post-Secondary - All team members must be currently enrolled in a Post-Secondary institution"/>
+              <List.Item description="Open - General Public, Mixed Student/Non-Student, Family (children under age 14 must be accompanied by parent/legal guardian)"/>
+            </List>
+            Note: A team must have a minimum of 4 members and a maximum of 6.<br/>
+            Note: A minimum of 15 teams are required to form a division; else the teams in that division will merge into the Open division.<br/>
+            Note: A WWU student may join an Open team, but not the reverse; else the WWU student team will become an Open division team<br/>
+          </Accordion.Content>
+
+          <Accordion.Title>
+            <Icon color="green" name="dropdown"/>
             How much does this cost?
-            <div className="sub header">
-              A small registration fee is being determined.
-            </div>
-          </div>
-        </h2>
-        <div className="ui attached segment">
-          A small fee of $5-10/person (for early bird registration) will be charged to cover costs of materials, prizes, food, campus reservations, etc.
-          We are seeking <a target="_blank" href="https://securelb.imodules.com/s/1710/campaign/index.aspx?sid=1710&gid=2&pgid=467&cid=1175&dids=167&bledit=1">donations</a> to help with expenses to keep fees minimized.
-        </div>
+          </Accordion.Title>
+          <Accordion.Content>
+            Registration Prices are per person.  All fees are non-refundable.  Fee is charged when person registers.<br/><br/>
+            <strong>Early Bird Registration Fee</strong><br/>
+            Open until March 10, 2017 <br/>
+            Student: $5<br/>
+            Non-Student: $10<br/><br/>
+            <strong>Regular Registration Fee</strong><br/>
+            Open from March 11 - March 30, 2017 <br/>
+            Student: $8<br/>
+            Non-Student: $15<br/>
+            Fees help cover costs of materials, prizes, food, campus services & reservations, etc.  We are seeking <a target="_blank" href="https://securelb.imodules.com/s/1710/campaign/index-noshare.aspx?sid=1710&gid=2&pgid=467&cid=1175&dids=167&bledit=1">donations</a> to help keep fees minimized.
+          </Accordion.Content>
 
-
-        {/* DO I HAVE TO DO MATH? */}
-        <h2 className="ui top attached header">
-          <i className="large icons">
-            <i className="olive calculator icon"></i>
-            <i className="green flask corner icon"></i>
-          </i>
-          <div className="content">
-            Do I have to be a math/ science person to do well?
-            <div className="sub header">Nope!</div>
-          </div>
-        </h2>
-        <div className="ui attached segment">
-          NO! A common misconception is that only mathematically inclined people are good at solving puzzles.
-          In this puzzle hunt, it will be to your advantage to have people on your team with
-          knowledge of music, art, humanities and social sciences, as well as science and mathematics.
-        </div>
-
-        {/* WHAT TO BRING? */}
-        <h2 className="ui top attached header">
-          <i className="teal suitcase icon"></i>
-          <div className="content">
+          <Accordion.Title>
+            <Icon color="teal" size="big" name="dropdown"/>
             What should I bring to the puzzle hunt?
-            <div className="sub header">Your creativity and problem solving skills!</div>
-          </div>
-        </h2>
-        <div className="ui attached segment">
-          <div className="ui list">
-            <div className="item">
-              <div className="ui horizontal red label">
-                <i className="large mobile icon"></i>Smart Phone
-              </div>
-              <strong>Required</strong> to submit puzzle codes to the website during the event.
-            </div>
-            <div className="item">
-              <div className="ui horizontal label">
-                <i className="large ion-android-clipboard icon"></i>Clipboard
-              </div>
-              Or notepad with graph paper might come in handy
-            </div>
-            <div className="item">
-              <div className="ui horizontal label">
-                <i className="large pencil icon"></i>Pencil
-              </div>
-              Or other writing utencils (highlighters, pens).
-            </div>
-            <div className="item">
-              <div className="ui horizontal label">
-                <i className="large ion-scissors icon"></i>Scissors
-              </div>
-              For cutting paper. Also tape!
-            </div>
-            <div className="item">
-              <div className="ui horizontal label">
-                <i className="large umbrella icon"></i>Umbrella
-              </div>
-              Not for you... for the problem packet/ paper.
-          </div>
-          </div>
-        </div>
+          </Accordion.Title>
+          <Accordion.Content>
+            Your creativity and problem solving skills! Along with the following:<br/>
+            <List>
+              <List.Item description="At least one smart phone.  The more the better! (Think about battery life)."/>
+              <List.Item description="A clip board, or a note pad.  Graph paper might come in handy."/>
+              <List.Item description="Scissors and tape."/>
+              <List.Item description="Writing utensils (pens and pencils)."/>
+              <List.Item description="A sturdy single hole punch (capable of punching through 8 layers of paper)."/>
+              <List.Item description="Water to drink! You are welcome to bring your own snacks too."/>
+            </List>
+          </Accordion.Content>
 
-        {/* WHAT IF IT RAINS? */}
-        <h2 className="ui top attached header">
-          <i className="large icons">
-            <i className="blue cloud icon"></i>
-            <i className="yellow lightning corner icon"></i>
-          </i>
-          <div className="content">
+          <Accordion.Title>
+            <Icon color="blue" size="big" name="dropdown"/>
+            Do I have to be a math/science person?
+          </Accordion.Title>
+          <Accordion.Content>
+            NO! A common misconception is that only mathematically inclined people are good at solving puzzles.
+            In this puzzle hunt, it will be to your advantage to have people on your team with
+            knowledge of music, art, humanities and social sciences, as well as science and mathematics.
+          </Accordion.Content>
+
+          <Accordion.Title>
+            <Icon color="violet" size="big" name="dropdown"/>
+            How long will this last? (Event Schedule)
+          </Accordion.Title>
+          <Accordion.Content>
+            <List>
+              <List.Item description="10:00 AM - Check in at Red Square.  Team's will receive a rules packet and any pre-ordered t-shirts."/>
+              <List.Item description="10:45 AM - Announcements."/>
+              <List.Item description="10:50 AM - Puzzle Hunt starts!"/>
+              <List.Item description="3:00 PM - Finishing Puzzles and Lunch"/>
+              <List.Item description="4:00 PM - Prizes awarded!"/>
+            </List>
+          </Accordion.Content>
+
+          <Accordion.Title>
+            <Icon color="purple" size="big" name="dropdown"/>
             What if it rains?
-            <div className="sub header">You live in Washington... it rains here.</div>
-          </div>
-        </h2>
+          </Accordion.Title>
+          <Accordion.Content>
+            Welcome to Washington.  We hunt on!
+          </Accordion.Content>
 
-        {/* HOW LONG? */}
-        <h2 className="ui top attached header">
-          <i className="violet clock icon"></i>
-          <div className="content">
-            How long will the puzzle hunt last?
-            <div className="sub header">10AM - 3PM</div>
-          </div>
-        </h2>
-        <div className="ui attached segment">
-          Registration and check-in will start at 10:00 a.m. in Red Square where your
-          team will receive a <a href="/pdfs/The Great Puzzle Hunt Rules of Play and Tips.pdf" target="_blank">rules packet</a> and team T-shirts pre-ordered by Date TBD.
-          The Puzzle Hunt will start at 11:00 a.m. when the first puzzle is released.
-          Prizes will be awarded at approximately 3:00 p.m.
-        </div>
+          <Accordion.Title>
+            <Icon color="pink" size="big" name="dropdown"/>
+            What kind of puzzles will we be solving?
+          </Accordion.Title>
+          <Accordion.Content>
+            Check out the <a href="/puzzles">puzzles from last year</a>!
+          </Accordion.Content>
 
-        {/* PUZZLES? */}
-        <h2 className="ui top attached header">
-          <i className="purple puzzle icon"></i>
-          <div className="content">
-            What kinds of puzzles will there be?
-            <div className="sub header"><a href="/puzzles">See examples</a></div>
-          </div>
-        </h2>
-        <div className="ui attached segment">
-          Each puzzle results in a code word or words that must be entered to stop the clock for that problem.
-          Time is a factor, so make sure everyone on the team is assigned to a job.
-          Some members can cut out objects if needed. If a problem has several parts, see if different
-          members can tackle different parts independently.
-          <br/><br/>
-          Once the proper code word(s) are entered, your team will be sent to the next destination.
-          If your team is stuck, you may ask for hints. Each hint will add time to your clock.
-          This information will be in the <a href="/pdfs/The Great Puzzle Hunt Rules of Play and Tips.pdf" target="_blank">rules packet</a>. Not all teams will travel the same paths.
-          At each destination, you will need to scan a QR code to receive your next puzzle and re-start the clock.
-        </div>
-
-        {/* T-Shirts? */}
-        <h2 className="ui top attached header">
-          <i className="pink ion-tshirt icon"></i>
-          <div className="content">
+          <Accordion.Title>
+            <Icon color="brown" size="big" name="dropdown"/>
             What do the T-Shirts look like?
-            <div className="sub header">Represent as a Puzzle Master!</div>
-          </div>
-        </h2>
-        <div className="ui attached segment">
-          <h3>Puzzle Hunt 2016 T-Shirts (2017 design on the way)</h3>
-          <br/><br/>
-          <div className="ui medium images">
-            <img className="ui image" src="img/T-Shirt-front.png"/>
-            <img className="ui image" src="img/T-Shirt-back.png"/>
-          </div>
-        </div>
+          </Accordion.Title>
+          <Accordion.Content>
+            This year we have awesome Next Level Poly/Cotton blend T-Shirts!<br/>
+            <List>
+              <List.Item>All colors are on <a target="_blank" href="http://www.nextlevelapparel.com/dsn/wwwnextlevelapparelcom/Content/PDF/2015ColorCard-web.pdf">Next Level's color chart</a> <strong>under the POLY/COTTON</strong> section.</List.Item>
+              <List.Item description="These are the kind of shirts you will want to wear all the time!"/>
+              <List.Item description="There is a women’s and a men’s version.  You can choose a White or Navy logo for any shirt color. (Some colors are only available for one gender)."/>
+              <List.Item description="Early bird price: $10."/>
+              <List.Item description="Regular Price (March 11 - March 23): $13"/>
+              <List.Item description="T-Shirt sales end March 23, 2017 at midnight."/>
+            </List>
+            <Image.Group>
+              <Image src="/img/2017/t-shirts/mens.jpg"/>
+              <Image src="/img/2017/t-shirts/womens.jpg"/>
+            </Image.Group>
+          </Accordion.Content>
+
+        </Accordion>
+
       </div>
     );
   }
