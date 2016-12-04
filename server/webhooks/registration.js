dcb901eb435cb03e7746c91974563fc2e39d0b29
Kyle Rader <kyle@kylerader.ninja>
2016-12-03 19:14:08 -0800

    Move helpers into imports folders

--- server/webhooks/registration.js
+++ server/webhooks/registration.js
@@ -2,7 +2,8 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { Email } from 'meteor/email';
 
-import { PostRoute } from '../post-route.js';
+import { PostRoute } from '../imports/post-route.js';
+import RegistrationParser from './imports/registration-parser.js';
 
 PostRoute.route('/api/register', function(params, req, res, next) {
 
