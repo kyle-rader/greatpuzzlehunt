841d136d348a8caa1ea92e0f6cd18aed7b993dd0
Kyle Rader <kyle@kylerader.ninja>
2016-12-10 16:41:35 -0800

    Add start of account setup form and isAdmin helper function

--- client/components/profile/accounts-setup.jsx
+++ client/components/profile/accounts-setup.jsx
@@ -1,19 +1,32 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
-import { Container, Form } from 'semantic-ui-react';
+import { Container, Form, Grid } from 'semantic-ui-react';
 
 AccountSetup = class AccountSetup extends Component {
+
   constructor(props) {
     super(props);
-
     this.state = {};
   }
 
   render() {
     return (
     <Container>
-      <PuzzlePageTitle title="Account Setup"/>
+      <PuzzlePageTitle title="Account Setup" subTitle="Welcome to Great Puzzle Hunt.  Please finish setting up your account :)" />
+      <Grid>
+        <Grid.Row>
+          <Grid.Column mobile="16" >
+            <Form size="big">
+              <Form.Input label="Choose a Username" name="username" placeholder="Username" />
+
+              <Form.Input label="Choose a Password" name="password1" placeholder="Password" />
+              <Form.Input label="Confirm Password" name="password2" placeholder="Confirm Password" />
+            </Form>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Container>
     );
   }
+
 }
