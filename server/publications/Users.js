e30ce1882267620dd45ca838339a901399869c2b
Kyle Rader <kyle@kylerader.ninja>
2016-12-11 16:35:22 -0800

    Publish all fields for individual users

--- server/publications/Users.js
+++ server/publications/Users.js
@@ -21,13 +21,25 @@ Meteor.publish(null, function() {
         name: 1,
         roles: 1,
         updatedAt: 1,
+        address: 1,
+        city: 1,
+        state: 1,
+        zip: 1,
+        age: 1,
+        email: 1,
+        name: 1,
+        phone: 1,
+        isAdult: 1,
+        registrationType: 1,
+        photoPermission: 1,
+        legalGuardian: 1,
       }
     });
   } else {
-      return this.ready();
+    return this.ready();
   }
 });
 
 Meteor.publish('admin.users', function(){
-  return isAdmin() ? Meteor.users.find({}) : [];
+  return isAdmin() ? Meteor.users.find({}) : this.ready();
 });
