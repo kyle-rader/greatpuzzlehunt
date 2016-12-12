a86be285a5e7f6417588e4c8dfe4771d7b287a00
Kyle Rader <kyle@kylerader.ninja>
2016-12-12 02:18:31 -0800

    Upgrade request password reset using semantic-ui-react

--- client/components/public/request-password-reset.jsx
+++ client/components/public/request-password-reset.jsx
@@ -1,111 +1,66 @@
-import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
-
-// Define our login comp
+import { Header, Grid, Segment, Form, Message } from 'semantic-ui-react';
 
 RequestPasswordReset = class RequestPasswordReset extends Component {
 
     constructor(props) {
       super(props);
       this.state = {
-        err: null,
-        email: null,
+        error: null,
+        username: '',
       };
     }
 
-    componentDidMount() {
-      let form = $(this.refs.resetForm);
-
-      // Init form validation and submission
-      $(form).form({
-        fields: {
-          username: {
-            identifier: 'username',
-            rules: [{
-              type: 'empty',
-              prompt: 'Enter your WWU Username'
-            }]
-          }
-        },
-        inline: true,
-        onSuccess: (event, fields) => {
-          event.preventDefault();
-
-          // Call Meteor method to create account.
-          Meteor.call('userSendPasswordReset', fields, (err, result) => {
-            if (err) {
-              this.setState({err: err});
-              console.log(err);
-            }
-            else {
-              this.setState({err: null, email: result.email});
-            }
-          });
-        }
-      });
-    }
-
-    getErrorMsg() {
-      if (this.state.err) {
-        return (
-        <div className="ui error message">
-          {this.state.err.reason}
-        </div>);
-      }
-    }
-
-    getSuccessMsg() {
-      if (this.state.email) {
-        return (
-          <div className="ui success message">
-            <h3>A password reset email has been sent to <strong>{this.state.email}</strong></h3>
-            <a className="ui facebook button" target="_blank" href="https://www.facebook.com/greatpuzzlehunt/">
-              <i className="facebook icon"></i>
-              Share the Puzzle Hunt!
-            </a>
-          </div>);
-      }
-    }
-
-    getForm() {
-      if (!this.state.email) {
-        return (
-          <form id="resetForm" className="ui huge form" ref="resetForm">
-            <div className="ui raised segment transparent-bg">
-              <h2 className="ui violet header">
-                <div className="content">
-                  Request Password Reset
-                </div>
-              </h2>
-              <div className="field">
-                <div className="ui left icon input">
-                  <i className="user icon"></i>
-                  <input type="text" name="username" placeholder="Username or Email" autoComplete="off" />
-                </div>
-              </div>
-
-              <input className="ui fluid large violet submit button" type="submit" value="Reset" />
-            </div>
-          </form>
-        );
-      }
-    }
-
     render() {
-
       return (
-      <div className="request-password-reset ui middle aligned center aligned grid custom-bg red-square">
-          <div className="column">
+      <Grid className="request-password-reset" textAlign='center' verticalAlign='middle'>
+        <Grid.Row columns="1">
+          <Grid.Column>
+            <Segment raised>
+              <Form onSubmit={(e) => this._handleSubmit(e)}>
+                <Header as="h2" color="violet" content="Request Password Reset"/>
+                <Form.Input name="username" placeholder="Username" value={this.state.username} onChange={(e) => this._handleChange(e)}/>
+                <Form.Button type='submit' color='violet' content='Reset' fluid/>
+                <Message
+                  negative
+                  hidden={!this.state.error}
+                  icon="warning sign"
+                  onDismiss={() => this.setState({ error: null })}
+                  content={this.state.error ? this.state.error.reason : ''}
+                />
+                <Message
+                  positive
+                  hidden={!this.state.email}
+                  icon="mail"
+                  onDismiss={() => this.setState({ email: null })}
+                  content={`A password reset email has been sent to ${this.state.email}`}
+                />
+              </Form>
+            </Segment>
 
-              {this.getForm()}
-              {this.getSuccessMsg()}
-              {this.getErrorMsg()}
+            <Message>
+              <Link to="/login">Log In</Link>
+            </Message>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>);
+    }
 
-              <div className="ui message">
-                <Link to="/login">Log In</Link>
-              </div>
-          </div>
-      </div>);
+    _handleChange(e) {
+      const { name, value } = e.target;
+      this.setState({ [name]: value });
+    }
+
+    _handleSubmit(e) {
+      e.preventDefault();
+
+      // Call Meteor method to create account.
+      Meteor.call('user.passwordReset', this.state.username, (error, result) => {
+        if (error) return this.setState({ error });
+
+        this.setState({ error: null, email: result.email, username: '' });
+      });
     }
 }
