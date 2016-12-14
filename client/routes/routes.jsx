6c1b764291ead0042b878e95c38d2babca55aa43
Kyle Rader <kyle@kylerader.ninja>
2016-12-14 09:24:32 -0800

    Start team/create component

--- client/routes/routes.jsx
+++ client/routes/routes.jsx
@@ -4,45 +4,50 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 export const renderRoutes = () => (
   <Router history={browserHistory}>
 
-    <Route path="/" component={App}>
+    <Route path='/' component={App}>
 
       {/*
         * Public Routes
       */}
 
       <IndexRoute component={Home}/>
-      <Route path="gallery" component={Gallery}/>
-      <Route path="contact" component={Contact}/>
-      <Route path="puzzles" component={Puzzles}/>
-      <Route path="info" component={Info}/>
-      <Route path="qrcode" component={QRCode}/>
+      <Route path='gallery' component={Gallery}/>
+      <Route path='contact' component={Contact}/>
+      <Route path='puzzles' component={Puzzles}/>
+      <Route path='info' component={Info}/>
+      <Route path='qrcode' component={QRCode}/>
 
       {/* Authentication Routes */}
-      <Route path="login" component={Login}/>
-      <Route path="register" component={Register}/>
-      <Route path="requestpasswordreset" component={RequestPasswordReset}/>
-      <Route path="passwordreset/:token" component={PasswordReset}/>
+      <Route path='login' component={Login}/>
+      <Route path='register' component={Register}/>
+      <Route path='requestpasswordreset' component={RequestPasswordReset}/>
+      <Route path='passwordreset/:token' component={PasswordReset}/>
 
-      <Route path="setup/:token" component={AccountSetup}/>
+      <Route path='setup/:token' component={AccountSetup}/>
 
       {/*
         * Authed Routes
       */}
 
-      <Route path="profile" component={Profile}/>
-
-      {/* Admin Routes */}
-      <Route path="admin" component={Admin}>
-        <IndexRoute component={AdminUserList} />
-        <Route path="users" component={AdminUserList} />
-        <Route path="teams" component={AdminTeamList} />
-        <Route path="puzzles" component={PuzzleDashboard} />
-        <Route path="email" component={BulkEmail} />
-        <Route path="game" component={GamePlay} />
-        <Route path="scoring" component={Scoring} />
+      <Route path='profile' component={Profile}/>
+      <Route path='team' >
+        {/* <IndexRoute component={Team}/> */}
+        <Route path='create' component={TeamCreator}/>
+        {/* <Route path='join' component={null}/> */}
       </Route>
 
-      <Route path="*" component={Home}/>
+      {/* Admin Routes */}
+      <Route path='admin' component={Admin}>
+        <IndexRoute component={AdminUserList} />
+        <Route path='users' component={AdminUserList} />
+        <Route path='teams' component={AdminTeamList} />
+        <Route path='puzzles' component={PuzzleDashboard} />
+        <Route path='email' component={BulkEmail} />
+        <Route path='game' component={GamePlay} />
+        <Route path='scoring' component={Scoring} />
+      </Route>
+
+      <Route path='*' component={Home}/>
     </Route>
   </Router>
 );
