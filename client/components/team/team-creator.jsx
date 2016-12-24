3ccda79bbe4ab48b4a8d5b6de80b179e13eae8b7
Kyle Rader <kyle@kylerader.ninja>
2016-12-24 00:02:15 -0500

    Add helper function to create make generic team comp

--- client/components/team/team-creator.jsx
+++ client/components/team/team-creator.jsx
@@ -2,11 +2,16 @@ import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import { Link, browserHistory } from 'react-router';
 import { Container, Form, Message, Header, Icon, Button } from 'semantic-ui-react';
+import { makeTeamComp } from './imports/team-helpers.js';
 
 TeamCreator = class TeamCreator extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('Getting new props', nextProps);
   }
 
   render() {
@@ -17,3 +22,5 @@ TeamCreator = class TeamCreator extends Component {
     );
   }
 }
+
+TeamCreator = makeTeamComp(TeamCreator);
