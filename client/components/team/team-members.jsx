5780d78354974c29c6ad689ed404c3c9d5337cd2
Kyle Rader <kyle@kylerader.ninja>
2016-12-30 23:27:48 -0800

    Fix bug in creating test users with teamId's

--- client/components/team/team-members.jsx
+++ client/components/team/team-members.jsx
@@ -23,7 +23,7 @@ TeamMembers = class TeamMembers extends Component {
             {member.name}
           </Card.Header>
           <Card.Meta>
-            {member.phone} <br/>{member.getEmail()}
+            {member.getEmail()}<br/>{member.phone}
           </Card.Meta>
         </Card.Content>
         <Card.Content extra>
--- server/publications/users.js
+++ server/publications/users.js
@@ -1,6 +1,28 @@
 import { Meteor } from 'meteor/meteor';
 import { isAdmin } from '../../lib/imports/method-helpers.js';
 
+const USER_FIELDS = {
+  emails: 1,
+  firstname: 1,
+  lastname: 1,
+  name: 1,
+  roles: 1,
+  updatedAt: 1,
+  address: 1,
+  city: 1,
+  state: 1,
+  zip: 1,
+  age: 1,
+  email: 1,
+  name: 1,
+  phone: 1,
+  isAdult: 1,
+  registrationType: 1,
+  photoPermission: 1,
+  legalGuardian: 1,
+  teamId: 1,
+};
+
 Meteor.users.deny({
   update: () => {
     return true;
@@ -14,31 +36,21 @@ Meteor.publish(null, function() {
     return Meteor.users.find({
       _id: userId
     }, {
-      fields: {
-        emails: 1,
-        firstname: 1,
-        lastname: 1,
-        name: 1,
-        roles: 1,
-        updatedAt: 1,
-        address: 1,
-        city: 1,
-        state: 1,
-        zip: 1,
-        age: 1,
-        email: 1,
-        name: 1,
-        phone: 1,
-        isAdult: 1,
-        registrationType: 1,
-        photoPermission: 1,
-        legalGuardian: 1,
-        teamId: 1,
-      }
+      fields: USER_FIELDS,
     });
   } else {
     return this.ready();
   }
+});
+
+Meteor.publish('users.myTeam', function() {
+  const { userId } = this;
+  if (!userId) return this.ready();
+
+  const user = Meteor.users.findOne({ _id: userId });
+  if (!user) return this.ready();
+
+  return Meteor.users.find({ teamId: user.teamId }, { fields: USER_FIELDS });
 });
 
 Meteor.publish('admin.users', function(){
