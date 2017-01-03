753091e4cb7b49050c24065d2da90133ec060acf
Kyle Rader <kyle@kylerader.ninja>
2017-01-02 19:50:28 -0800

    Implement 5 min debounced invite resend button

--- client/components/team/team-invites.jsx
+++ client/components/team/team-invites.jsx
@@ -1,17 +1,31 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
-import { Card, Icon, Button } from 'semantic-ui-react';
+import { Card, Icon, Button, Message } from 'semantic-ui-react';
 import moment from 'moment';
 
 TeamInvites = class TeamInvites extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      success: null,
+    };
   }
 
   render() {
     if (this.props.ready) {
-      return <Card.Group>{this._renderInvites()}</Card.Group>;
+      return (
+        <Card.Group>
+          {this._renderInvites()}
+          <Message
+           positive
+           hidden={!this.state.success}
+           icon="check"
+           onDismiss={() => this.setState({ success: null })}
+           content={this.state.success}
+          />
+        </Card.Group>
+      );
     }
     return <Loading />;
   }
@@ -28,11 +42,22 @@ TeamInvites = class TeamInvites extends Component {
           </Card.Meta>
         </Card.Content>
         <Card.Content extra>
-          <Button floated='left' icon='mail' color='green' inverted content='Resend'/>
+          <Button floated='left' icon='mail' color='green' inverted content='Resend' onClick={() => this._handleResendClick(invite.email)}/>
           <Button floated='right' icon='trash' color='red' inverted content='Revoke'/>
         </Card.Content>
       </Card>
     ));
+  }
+
+  _handleResendClick(email) {
+    Meteor.call('teams.inviteMember', this.props.team, email, (error, result) => {
+      if (error) return this.setState({ error });
+
+      this.setState({ success: `Invite resent to ${email}` });
+      Meteor.setTimeout(() => {
+        this.setState({ success: null });
+      }, 6000);
+    });
   }
 }
 
