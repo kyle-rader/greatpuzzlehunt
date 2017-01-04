9c93953a6462d4f1024f81393de79ab28d4ac4df
Kyle Rader <kyle@kylerader.ninja>
2017-01-03 21:56:42 -0800

    Display team invites in no team message component

--- server/publications/invites.js
+++ server/publications/invites.js
@@ -9,3 +9,13 @@ Meteor.publish('teams.invites', function() {
 
   return Invites.find({ teamId: user.teamId });
 });
+
+Meteor.publish('invites.myInvites', function() {
+  const { userId } = this;
+  if (!userId) return this.ready();
+
+  const user = Meteor.users.findOne({ _id: userId });
+  if (!user) return this.ready();
+
+  return Invites.find({ email: user.getEmail() });
+});
