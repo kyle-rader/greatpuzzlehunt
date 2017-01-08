1697478b7663e85fb7f52f0db903cb16fb3f646a
Kyle Rader <kyle@kylerader.ninja>
2017-01-08 13:14:44 -0800

    Add team list card password field

--- client/components/team/team-list-card.jsx
+++ client/components/team/team-list-card.jsx
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
-import { Card, Icon, Button, Progress } from 'semantic-ui-react';
+import { Card, Icon, Button, Progress, Form } from 'semantic-ui-react';
 import moment from 'moment';
 
 import { DEVISION_MAP } from './imports/team-helpers.js';
@@ -13,14 +13,14 @@ TeamListCard = class TeamListCard extends Component {
     this.state = {
       isFull: team.members.length >= 6,
       memberCount: team.members.length,
-      updatedAt: moment(team.updatedAt).fromNow(),
       division: DEVISION_MAP[team.division],
       showPasswordField: false,
+      password: '',
     };
   }
 
   render() {
-    const { division, showPasswordField, isFull, memberCount, updatedAt } = this.state;
+    const { division, showPasswordField, isFull, memberCount } = this.state;
     const { team } = this.props;
     const membersLabel = `${memberCount} of 6 members`;
     const membersPercent = Math.round((memberCount / 6)*100);
@@ -28,17 +28,44 @@ TeamListCard = class TeamListCard extends Component {
     return (
       <Card centered>
         <Card.Content>
-          <Card.Header>{team.name}</Card.Header>
-          <Card.Meta><Icon name='sitemap'/> { division } </Card.Meta>
-          <Card.Description><Progress size='small' percent={membersPercent} color={progressColor}>{membersLabel}</Progress></Card.Description>
+          <Card.Header>{ team.name }</Card.Header>
+          <Card.Meta><Icon name='sitemap'/> { division }</Card.Meta>
+          <Card.Description>
+            <Progress size='small' percent={ membersPercent } color={progressColor}>{ membersLabel }</Progress>
+          </Card.Description>
         </Card.Content>
         <Card.Content extra>
-          Updated<br/>
-          { updatedAt }
-          {!isFull ? <Button size='small' floated='right' icon='reply' labelPosition='right' content='Join Team'/> : null}
+          { !isFull && !showPasswordField ?
+            <Button size='small' floated='right' icon='reply' labelPosition='right' content='Join Team' onClick={() => this.setState({ showPasswordField: true })}/>
+            : null
+          }
+          { !isFull && showPasswordField ?
+            this._renderPasswordField()
+            : null
+          }
         </Card.Content>
       </Card>
     );
+  }
+
+  _renderPasswordField() {
+    return (
+      <Form onSubmit={(e) => this._handlePasswordSubmit(e)}>
+        <Form.Input name='password' label='Team Password' value={ this.state.password } onChange={(e) => this.setState({ password: e.target.value })}/>
+        <Button color='green' type='submit' content='Submit' size='small'/>
+        <Button floated='right' color='red' inverted content='Cancel' size='small' onClick={(e) => this._cancel(e)}/>
+      </Form>
+    );
+  }
+
+  _handlePasswordSubmit(e) {
+    e.preventDefault();
+    console.log(`Join team ${this.props.team.name} with password ${this.state.password}`);
+  }
+
+  _cancel(e) {
+    e.preventDefault();
+    this.setState({ password: '', showPasswordField: false });
   }
 }
 
