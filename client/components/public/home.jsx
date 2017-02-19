f0eb6687bbbd9e399b67b3295786edd6d85d0c86
Kyle Rader <kyle@kylerader.ninja>
2017-02-18 23:15:29 -0800

    refactor gallery preview

--- client/components/public/home.jsx
+++ client/components/public/home.jsx
@@ -12,23 +12,9 @@ import EventDescription2 from './imports/event-description-2';
 import RegistrationInfo from './imports/registration-info';
 import SamplePuzzles from './imports/sample-puzzles';
 import ProfileCards from './imports/profile-cards';
+import GalleryPreview from './imports/gallery-preview';
 
 Home = class Home extends Component {
-
-  _renderGallerySection() {
-    return (
-    <div className="one column stacking row">
-      <div className="dark-blue column">
-        <div className="ui very padded basic segment">
-          <h1>Take a look at past events</h1>
-          <h3>Photography courtesy of Gabrielle Poncz</h3>
-          <Link className="ui large white button" to="/gallery">View Gallery</Link>
-        </div>
-      </div>
-    </div>
-    );
-  }
-
   render() {
     return (
     <Container>
@@ -43,25 +29,7 @@ Home = class Home extends Component {
       <RegistrationInfo/>
       <SamplePuzzles/>
       <ProfileCards />
-
-      <div className="ui padded stackable filling grid">
-
-
-        {this._renderGallerySection()}
-
-        <div className="three column stacking row">
-          <div className="no-padding column">
-            <img className="ui image" src="/img/2016/event-photos/team-church-of-put-it-backism.jpg"/>
-          </div>
-          <div className="no-padding column">
-            <img className="ui image" src="/img/2016/event-photos/team-finesse.jpg"/>
-          </div>
-          <div className="no-padding column">
-            <img className="ui image" src="/img/2016/event-photos/team-mod.jpg"/>
-          </div>
-        </div>
-
-      </div>
+      <GalleryPreview/>
 
     </Container>
     );
