9af9666eb99e5a624dc41c57c02a3f7eadd5eed9
Kyle Rader <kyle@kylerader.ninja>
2017-02-18 19:27:02 -0800

    Add LinkButton component

---
+++ client/components/imports/link-button.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { Button } from 'semantic-ui-react';
+
+export default class LinkButton extends Component {
+  render() {
+    const linkStyle = {
+      marginBottom: '5px',
+      display: 'inline-block',
+    };
+
+    if (this.props.as) {
+      return <Button as={this.props.as} style={linkStyle} size={this.props.size} color={this.props.color} content={this.props.content} icon={this.props.icon}/>
+    }
+
+    return (
+      <Link to={this.props.to} style={linkStyle}>
+        <Button size={this.props.size} color={this.props.color} content={this.props.content} icon={this.props.icon}/>
+      </Link>
+    )
+  }
+}
