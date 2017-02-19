ee58a1e5b166e836ddf79c9b70671ad1ecdc4051
Kyle Rader <kyle@kylerader.ninja>
2017-02-18 19:27:42 -0800

    add volunteer page

---
+++ client/components/public/volunteer.jsx
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+import { Container, Header, Segment, Accordion, Button, Icon } from 'semantic-ui-react';
+
+Volunteer = class Volunteer extends Component {
+  render() {
+    return (
+      <Container>
+        <br/>
+        <iframe src="https://docs.google.com/a/kylerader.ninja/forms/d/e/1FAIpQLSfTwicPiiM21gQE-nRvDJm5vWPQyLhshkZrykRwtKygTWlhdw/viewform?embedded=true" width="100%" height="800px" frameBorder="0" marginHeight="0" marginWidth="0">Loading...</iframe>
+      </Container>
+    )
+  }
+}
