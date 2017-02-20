840bbd40e0662005ab72f9c509dce08dec29407d
Kyle Rader <kyle@kylerader.ninja>
2017-02-19 23:00:55 -0800

    conect user delete action, prevent users on teams from being deleted

--- client/components/admin/users/imports/admin-user.jsx
+++ client/components/admin/users/imports/admin-user.jsx
@@ -21,15 +21,16 @@ class AdminUser extends Component {
             onEdit={ (e) => this._toggleEdit() }
             onPasswordReset={(e) => this._sendPasswordReset(e)}
             onEmailResend={(e) => this._sendEmailResend(e)}
-            onDelete={ (e) => console.log(`delete: ${this.props.user.name}`) }
+            onDelete={ (e) => this._deleteUser(e) }
           />
         </Grid.Column>
       </Grid.Row>
     );
   }
 
-  _toggleEdit() {
-    this.setState({ editMode: !this.state.editMode });
+  _toggleEdit(state = null) {
+    const newState = state !== null ? Boolean(state) : !this.state.editMode
+    this.setState({ editMode: newState });
   }
 
   _sendPasswordReset(event) {
@@ -80,148 +81,18 @@ class AdminUser extends Component {
     });
   }
 
-  saveUser(event) {
-    const firstname = this.refs.firstname.value;
-    const lastname = this.refs.lastname.value;
-    const username = this.refs.username.value;
-    const email = this.refs.email.value;
-
-    console.log(`Tryin to save \nfirst:${firstname}\nlast:${lastname}\nUsername:${username} \nEmail:${email} `);
-
-    const btn = $(event.target);
-
-    Meteor.call('userAdminUpdate', {
-        _id: this.props.user._id,
-      firstname: firstname,
-      lastname: lastname,
-      username: username,
-      email: email
-    }, (err, result) => {
-      if (err) {
-        console.log(err);
-        btn.attr('data-content', 'Failed to save user! 😰');
-      } else {
-        btn.attr('data-content', `${firstname} saved! 😀`);
-      }
-
-      btn.popup({
-        on: 'manual'
-      }).popup('show');
-      setTimeout(() => {
-        btn.popup('hide');
-      }, 3000);
-    });
-    this.setState({ editMode: false });
-  }
-
-  toggleVolunteer(event) {
-
-    if(!confirm(`Are you sure you want to toggle ${this.props.user.name} as a Volunteer?`))
+  _deleteUser(event) {
+    if (!confirm(`Confirm DELETE "${this.props.user.name}" !?!?`))
       return;
-
-    const btn = $(event.target);
-
-    Meteor.call('userPromoteToVolunteer', {_id: this.props.user._id}, (err, result) => {
-      if (err) {
-        console.log(err);
-        btn.attr('data-content', 'Toggle Failed! 😰');
-      } else {
-        btn.attr('data-content', `${this.props.user.firstname} toggled! 😀`);
-      }
-
-      btn.popup({
-        on: 'manual'
-      }).popup('show');
-      setTimeout(() => {
-        btn.popup('hide');
-      }, 3000);
-    });
-  }
-
-  deleteUser(event) {
-    if (!confirm(`Are you sure you want to DELETE ${this.props.user.name}!?!?`))
-        return;
 
     let btn = $(event.target);
 
-    Meteor.call('userAdminDelete', {
-        _id: this.props.user._id
-    }, (err, result) => {
-        if (err) {
-            console.log(err);
-            btn.attr('data-content', 'Failed to delete user! 😰');
-        } else {
-            btn.attr('data-content', 'Deleted! 😀');
-        }
-
-        btn.popup({
-            on: 'manual'
-        }).popup('show');
-        setTimeout(() => {
-            btn.popup('hide');
-        }, 3000);
+    Meteor.call('userAdminDelete', this.props.user._id, (err, result) => {
+      if (err) {
+        alert(err);
+        btn.attr('data-content', 'Failed to delete user! 😰');
+      }
     });
-  }
-
-  getName() {
-    const user = this.props.user;
-    if (!this.state.editMode) return <td>{ user.name }</td>;
-
-    return (
-      <td>
-        <div className="ui small form">
-          <div className="field">
-            <input ref="firstname" type="text" defaultValue={this.props.user.firstname}/>
-          </div>
-          <div className="field">
-            <input ref="lastname" type="text" defaultValue={this.props.user.lastname}/>
-          </div>
-        </div>
-      </td>
-    );
-  }
-
-  getUsername() {
-    const user = this.props.user;
-    if (!this.state.editMode) return <td>{ this.props.user.username }</td>;
-
-    return (
-      <td>
-        <div className="ui small fluid input">
-          <input ref="username" type="text" defaultValue={this.props.user.username}/>
-        </div>
-      </td>
-    );
-  }
-
-  getEmail() {
-    const user = this.props.user;
-    const verified = user.getEmail() ? user.emails[0].verified : false;
-    const email = user.getEmail() || user.email;
-    if (this.state.editMode) {
-      return (
-        <td>
-          <div className="ui small fluid input">
-            <input ref="email" type="text" defaultValue={ email }/>
-          </div>
-        </td>
-      );
-    } else {
-      const verifyBtn = !verified ? <div className="ui right floated yellow basic tiny compact icon button" title="Send Verification Email" onClick={this.verifyEmail.bind(this)}><i className="send icon"></i></div> : null;
-      const rolesBtn = (
-        <div className={`ui right floated ${user.roles.indexOf('volunteer') >= 0 ? 'yellow' : 'gray'} basic tiny compact icon button`} title="Toggle Volunteer Role" onClick={this.toggleVolunteer.bind(this)}>
-          <i className="heart icon"></i>
-        </div>
-      );
-
-      return (
-        <td className={verified ? 'positive' : 'negative'}>
-          { rolesBtn }
-          { verifyBtn }
-          { email } &nbsp;
-        </td>
-      );
-    }
   }
 
   componentWillReceiveProps() {
