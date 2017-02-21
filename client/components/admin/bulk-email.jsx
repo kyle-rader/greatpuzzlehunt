a467b97e8890ee2c20db4a9d688dd8b00645c87e
Kyle Rader <kyle@kylerader.ninja>
2017-02-21 02:09:19 -0800

    Upgrade bulk email to react semantic-ui

--- client/components/admin/bulk-email.jsx
+++ client/components/admin/bulk-email.jsx
@@ -1,70 +1,70 @@
-import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import React, { Component } from 'react';
+import { Container, Message } from 'semantic-ui-react';
 
-export default BulkEmail = React.createClass({
+BulkEmail = class BulkEmail extends Component {
+  render() {
+    return (
+      <form ref="emailForm" className="ui segment form">
+          <h3 className="ui header">Bulk Email</h3>
+          <div className="fields">
+              <div className="twelve wide field">
+                  <label>Subect</label>
+                  <input ref="subject" type="text" className="ui fluid input" />
+              </div>
+              <div className="four wide field">
+                  <label>Group</label>
+                  <select ref="role" className="ui dropdown">
+                      <option value="user">Users</option>
+                      <option value="volunteer">Volunteers</option>
+                      <option value="admin">Admins</option>
+                  </select>
+              </div>
+          </div>
+          <div className="field">
+              <textarea ref="message" rows="6"></textarea>
+          </div>
+              <input type="submit" className="ui blue button" value="Send" onClick={this.sendEmail}/>
+      </form>
+    );
+  }
 
-    sendEmail(event) {
-        event.preventDefault();
+  componentDidMount() {
+    $(this.refs.role).dropdown();
+    $(this.refs.emailForm).form();
+  }
 
-        if (!confirm(`You are about to send a bulk email to everyone in the ${this.refs.role.value} group!`))
-            return;
+  sendEmail(event) {
+    event.preventDefault();
 
-        let fields = {
-            role: this.refs.role.value,
-            subject: this.refs.subject.value,
-            text: this.refs.message.value
-        };
+    if (!confirm(`You are about to send a bulk email to everyone in the ${this.refs.role.value} group!`))
+      return;
 
-        let btn = $(event.target);
+    const fields = {
+      role: this.refs.role.value,
+      subject: this.refs.subject.value,
+      text: this.refs.message.value
+    };
 
-        Meteor.call('userAdminBulkEmail', fields, (err, result) => {
-            if (err) {
-                console.log(err);
-                btn.attr('data-content', 'Failed to send email! 😰');
-            } else {
-                btn.attr('data-content', `Bulk email sent! 😀`);
-                $(this.refs.emailForm).form('reset');
-            }
+    const btn = $(event.target);
 
-            btn.popup({
-                on: 'manual'
-            }).popup('show');
-            setTimeout(() => {
-                btn.popup('hide');
-            }, 2500);
-        });
+    Meteor.call('admin.bulkEmail', fields, (err, result) => {
+      if (err) {
+        console.log(err);
+        btn.attr('data-content', 'Failed to send email! 😰');
+      } else {
+        btn.attr('data-content', `Bulk email sent! 😀`);
+        $(this.refs.emailForm).form('reset');
+      }
 
-    },
+      btn.popup({
+        on: 'manual'
+      }).popup('show');
+      setTimeout(() => {
+        btn.popup('hide');
+      }, 2500);
+    });
 
-    componentDidMount() {
-        $(this.refs.role).dropdown();
-        $(this.refs.emailForm).form();
-    },
+  }
 
-    render() {
-
-        return (
-        <form ref="emailForm" className="ui segment form">
-            <h3 className="ui header">Bulk Email</h3>
-            <div className="fields">
-                <div className="twelve wide field">
-                    <label>Subect</label>
-                    <input ref="subject" type="text" className="ui fluid input" />
-                </div>
-                <div className="four wide field">
-                    <label>Group</label>
-                    <select ref="role" className="ui dropdown">
-                        <option value="user">Users</option>
-                        <option value="volunteer">Volunteers</option>
-                        <option value="admin">Admins</option>
-                    </select>
-                </div>
-            </div>
-            <div className="field">
-                <textarea ref="message" rows="6"></textarea>
-            </div>
-                <input type="submit" className="ui blue button" value="Send" onClick={this.sendEmail}/>
-        </form>
-        );
-    }
-});
+}
