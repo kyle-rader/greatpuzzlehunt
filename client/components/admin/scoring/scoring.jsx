79a05c200401c8f471289de94e1b97df377220c1
Kyle Rader <kyle@kylerader.ninja>
2017-02-21 02:05:42 -0800

    Purge scoring

--- client/components/admin/scoring/scoring.jsx
+++ client/components/admin/scoring/scoring.jsx
@@ -1,40 +1,14 @@
-import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import React, { Component } from 'react';
+import { Container, Message } from 'semantic-ui-react';
 
-export default Scoring = React.createClass({
-
-    mixins:[ReactMeteorData],
-    getMeteorData() {
-        let data = {};
-
-        let teamsHandle = Meteor.subscribe('teams.all');
-        let loading = !teamsHandle.ready();
-
-        if (!loading) {
-            data.teams = Teams.find({}).fetch();
-        }
-
-        return data;
-    },
-
-    getTeamList() {
-        return this.data.teams.map((team) => {
-            return (
-                <TeamScore key={team._id} team={team} />
-            );
-        });
-    },
-
-    render() {
-
-        if (this.data.teams) {
-            return (
-            <div className="ui basic segment">
-                {this.getTeamList()}
-            </div>
-            );
-        } else {
-            return <Loading />;
-        }
-    }
-});
+Scoring = class Scoring extends Component {
+  render() {
+    return (
+      <Container>
+        <PuzzlePageTitle title='Scoring'/>
+        <Message info content='Scoring coming soon...'/>
+      </Container>
+    );
+  }
+}
