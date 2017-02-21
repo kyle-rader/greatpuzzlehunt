aa2808be446a3adbc7fb8cdf677c46fcfc02cedb
Kyle Rader <kyle@kylerader.ninja>
2017-02-21 02:01:11 -0800

    Purge that raw html shit

--- client/components/admin/puzzles/puzzle-dashboard.jsx
+++ client/components/admin/puzzles/puzzle-dashboard.jsx
@@ -1,114 +1,14 @@
-import React from 'react';
 import { Meteor } from 'meteor/meteor';
+import React, { Component } from 'react';
+import { Container, Message } from 'semantic-ui-react';
 
-PuzzleDashboard = React.createClass({
-    getInitialState() {
-        return {
-            puzzleToEdit: null
-        };
-    },
-
-    mixins:[ReactMeteorData],
-    getMeteorData() {
-        let data = {};
-
-        let puzzlesHandle = Meteor.subscribe('puzzles.all');
-        let loading = !puzzlesHandle.ready();
-
-        if (!loading) {
-            data.puzzles = PuzzleCollection.find({}).fetch()
-        }
-
-        return data;
-    },
-
-    createPuzzle(event) {
-        Meteor.call('createPuzzle');
-    },
-
-    setEditPuzzle(puzzle) {
-        if (this.state.puzzleToEdit) {
-            this.clearPuzzle();
-        }
-        else {
-            this.setState({
-                puzzleToEdit: puzzle._id
-            });
-        }
-    },
-
-    clearPuzzle() {
-        this.setState({
-            puzzleToEdit: null
-        });
-    },
-
-    renderPuzzleTable() {
-
-        if (this.data.puzzles) {
-            let puzzles = this.data.puzzles.map((puzzle) => {
-                return (
-                <tr key={puzzle._id}>
-                    <td>{puzzle.name}</td>
-                    <td>{puzzle.location}</td>
-                    <td>{puzzle.order}</td>
-                    <td>
-                        <div className="ui right floated blue button" onClick={this.setEditPuzzle.bind(this, puzzle)}>
-                            Edit
-                        </div>
-                    </td>
-                </tr>
-                );
-            });
-
-            return (
-                <table className="ui compact celled table">
-                    <thead>
-                        <tr>
-                            <th>Puzzle Name</th>
-                            <th>Location</th>
-                            <th>Order</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {puzzles}
-                    </tbody>
-                    <tfoot className="full-width">
-                        <tr>
-                            <th colSpan="4">
-                                <div className="ui right floated green button" onClick={this.createPuzzle}>
-                                    <i className="plus icon"></i> &nbsp; New Puzzle
-                                </div>
-                                <div className="ui right floated gray button" onClick={this.clearPuzzle}>
-                                    <i className="close icon"></i> &nbsp; Close
-                                </div>
-                            </th>
-                        </tr>
-                    </tfoot>
-                </table>);
-        } else {
-            return <div className="ui warning message">There are no puzzles yet!</div>;
-        }
-    },
-
-    render() {
-
-        let puzzle = PuzzleCollection.findOne({_id: this.state.puzzleToEdit});
-        let puzzleEdit;
-        if (puzzle) {            
-            puzzleEdit = <PuzzleEditor puzzle={puzzle} />
-        }
-
-        return (
-            <div className="basic segment">
-                <h3 className="ui header">Puzzle Creator</h3>
-                {puzzleEdit}
-                { this.renderPuzzleTable() }
-            </div>
-        );
-    }
-
-});
-
-export default PuzzleDashboard;
+PuzzleDashboard = class PuzzleDashboard extends Component {
+  render() {
+    return (
+      <Container>
+        <PuzzlePageTitle title='Puzzles'/>
+        <Message info content='Puzzle Editor coming soon...'/>
+      </Container>
+    );
+  }
+}
