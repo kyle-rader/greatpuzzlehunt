e8b5e5fe1a271efbeafff9f488cd1f42eb2e09e5
Kyle Rader <kyle@kylerader.ninja>
2017-03-04 22:35:01 -0800

    start promo registration form

--- client/components/public/register-promo-code.jsx
+++ client/components/public/register-promo-code.jsx
@@ -1,13 +1,21 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
-import { Container, Header, Segment, Accordion, Button, Icon } from 'semantic-ui-react';
+import { Container, Grid } from 'semantic-ui-react';
 import { Link } from 'react-router';
+
+import PromoRegisterForm from './imports/promo-register-form';
 
 RegisterPromoCode = class RegisterPromoCode extends Component {
   render() {
     return (
       <Container>
         <PuzzlePageTitle title='Promo Code Registration'/>
+
+        <Grid>
+          <Grid.Column>
+            <PromoRegisterForm />
+          </Grid.Column>
+        </Grid>
       </Container>
     );
   }
