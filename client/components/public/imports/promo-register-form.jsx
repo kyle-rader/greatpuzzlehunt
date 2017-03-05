e2a09dfc3a182696807d6f198cab5c9ec7448c75
Kyle Rader <kyle@kylerader.ninja>
2017-03-05 00:37:38 -0800

    add hold harmless agreement

--- client/components/public/imports/promo-register-form.jsx
+++ client/components/public/imports/promo-register-form.jsx
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
-import { Form, Button, Icon, List, Message } from 'semantic-ui-react';
+import { Segment, Form, Button, Icon, List, Message } from 'semantic-ui-react';
 
 const STATES = [
   { key: 'WA', text: 'WA', value: 'WA' },
@@ -84,6 +84,7 @@ export default class PromoRegisterForm extends Component {
       ecRelation: '',
       ecPhone: '',
       ecAltPhone: '',
+      holdHarmless: false,
     };
   }
 
@@ -138,7 +139,12 @@ export default class PromoRegisterForm extends Component {
           <Form.Select name='state' label='State' options={ STATES } value={ this.state.state } onChange={ (e,data) => this._handleDataChange(e,data) }/>
         </Form.Group>
 
-        <Form.Checkbox toggle name='photoPermission' label='Photo Permission (see below)' value={ this.state.promocode } onChange={ (e,data) => this._handleDataChange(e,data) }/>
+        <Form.Checkbox
+          toggle
+          name='photoPermission'
+          label='I hereby give my permission to Western to use my (or my minor child’s) image, in whole or in part, for public information and marketing of the WWU Great Puzzle Hunt at its discretion'
+          onChange={ (e,data) => this._handleDataChange(e,data) }
+        />
 
         <h3><Icon name='first aid' color='red' size='big'/>Emergency Contact <small>(optional)</small></h3>
         <Form.Group widths='equal'>
@@ -150,6 +156,16 @@ export default class PromoRegisterForm extends Component {
           <Form.Input name='ecPhone' label='Primary Phone' placeholder='111-222-3344' value={ this.state.ecPhone } onChange={ (e) => this._handleTextChange(e) }/>
           <Form.Input name='ecAltPhone' label='Alternative Phone' placeholder='123-123-1234' value={ this.state.ecAltPhone } onChange={ (e) => this._handleTextChange(e) }/>
         </Form.Group>
+
+        <h3><Icon name='pencil' color='orange' size='big'/>Acknowledgement of Risk & Hold Harmless Agreement</h3>
+        { this._holdHarmlessButton() }
+        { this._holdHarmless() }
+        <Form.Checkbox
+          toggle
+          name='holdHarmless'
+          label='By checking this box I acknowledge that I have read and understand the risk & hold harmless agreement.'
+          onChange={ (e,data) => this._handleDataChange(e,data) }
+        />
 
         <Form.Button type='submit' content='Register' color='green'/>
         <h3>Important Registration Information</h3>
@@ -189,6 +205,27 @@ export default class PromoRegisterForm extends Component {
   _handleCheckBoxChange(e) {
     const { name, value } = e.target;
     console.log(name, value);
+  }
+
+  _holdHarmlessButton() {
+    if (this.state.showHoldHarmless) {
+      return <Button basic content='Hide Agreement' onClick={ (e) => this.setState({ showHoldHarmless: false }) }/>
+    }
+    else {
+      return <Button basic content='Show Agreement' onClick={ (e) => this.setState({ showHoldHarmless: true }) }/>
+    }
+  }
+
+  _holdHarmless() {
+    if (!this.state.showHoldHarmless) return <p></p>;
+    return (
+      <Segment basic>
+        <p>I hereby acknowledge that I have voluntarily chosen (or voluntarily chosen to allow my minor child) to participate in the WWU Great Puzzle Hunt 2017 sponsored by the WWU Mathematics Department, held on April 1, 2017 (hereinafter referred to as “Puzzle Hunt”).  I understand the risks involved in the Puzzle Hunt, including the unlikely but potential risk of injury to me (or my minor child), and I agree to accept any and all risks associated with my participation.</p>
+        <p>In consideration of my (or my minor child’s) voluntary participation in the Puzzle Hunt, I agree to hold harmless Western Washington University, its officers, agents, volunteers, or employees from and against all financial loss, claim, suit, action, damage, or expense, arising out of my (or my minor child’s) participation, unless caused by the negligence or willful misconduct of the University, its officers, agents, volunteers, or employees.</p>
+        <p>I understand that Western Washington University strongly recommends that participants have comprehensive health insurance that provides essential health benefits as required by the Affordable Care Act (ACA).</p>
+        <p>I understand and acknowledge that a medical emergency may develop which necessitates the need for immediate medical treatment for a participant.  I hereby authorize Western and its officers, agents, volunteers or employees to arrange or provide any necessary emergency medical treatment on my (or my minor child’s) behalf.</p>
+      </Segment>
+    );
   }
 
 }
