7ea9e6a6fe5953f2d43f7dc38e04b45300cb38ec
Kyle Rader <kyle@kylerader.ninja>
2017-03-20 21:56:06 -0700

    Add Team members list to admin team list

--- client/components/admin/teams/team-list.jsx
+++ client/components/admin/teams/team-list.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
-import { Container, Grid } from 'semantic-ui-react';
+import { Container, Grid, Segment } from 'semantic-ui-react';
 
 AdminTeamList = class AdminTeamList extends Component {
   render() {
@@ -9,43 +9,13 @@ AdminTeamList = class AdminTeamList extends Component {
     return (
       <Container>
         <PuzzlePageTitle title='Teams'/>
-        <div className="ui basic segment">
-          <div className="ui grid">
-              <div className="six wide column">
-                  <div className="ui large blue label">
-                      <i className="users icon"></i>
-                      &nbsp;
-                      {this.props.teams.length} Teams
-                  </div>
-              </div>
-              <div className="ten wide column">
-                  <div className="ui fluid right icon input">
-                      <input type="text" placeholder="Search" onChange={this._search}/>
-                      <i className="search icon"></i>
-                  </div>
-              </div>
-              <div className="three wide column">
-              </div>
-          </div>
-          { this._getTeamList() }
-        </div>
+        { this._getTeamList() }
       </Container>
     );
   }
 
   _getTeamList() {
     return this.props.teams.map((team) => <AdminTeamListRow key={team._id} team={team} />);
-  }
-
-  _search(event) {
-    const search = event.target.value;
-    $(`div.ui.team-listing:contains(${search})`).each(function() {
-      $(this).show();
-    });
-
-    $(`div.ui.team-listing:not(:contains(${search}))`).each(function() {
-      $(this).hide();
-    });
   }
 
 }
