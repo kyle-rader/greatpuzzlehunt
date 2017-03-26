870237a5dc963b946240eb1575bc819488b59c79
Kyle Rader <kyle@kylerader.ninja>
2017-03-26 05:55:08 -0700

    Use spaces only. Return user prop in game-access

--- client/components/game/game.jsx
+++ client/components/game/game.jsx
@@ -2,25 +2,26 @@ import React, { Component } from 'react';
 import { Message } from 'semantic-ui-react';
 import GamestateComp from '../imports/gamestate-comp';
 
-
 Game = class Game extends Component {
-	render() {
-		if (this.props.gamestate.gameplay) {
-			this._renderMain();
-		}
-		else {
-			return <Message
+  render() {
+    if (this.props.gamestate.gameplay) {
+      this._renderMain();
+    }
+    else {
+      return <Message
         info
         header='Game Not Running'
-        content='Indication Gameplay is Currently Off' />
-		}
-	}
+        content='Indication Gameplay is Currently Off'
+      />
+    }
+  }
 
-	_renderMain() {
-		return (
-			<GameAccess />
-		);
-	}
+  _renderMain() {
+    return (
+      <GameAccess />
+    );
+  }
+
 }
 
 Game = GamestateComp(Game);
