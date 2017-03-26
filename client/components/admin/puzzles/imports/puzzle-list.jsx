1f5a07c12c45dae9541136fe977195e62f8c09af
Kyle Rader <kyle@kylerader.ninja>
2017-03-25 23:59:30 -0700

    Implement Puzzle editor (without hints)

--- client/components/admin/puzzles/imports/puzzle-list.jsx
+++ client/components/admin/puzzles/imports/puzzle-list.jsx
@@ -5,20 +5,33 @@ import { Grid, Label, Button } from 'semantic-ui-react';
 
 import puzzleSubscriber from './puzzle-subscriber';
 
-const stageColorMap = {
-  1: 'green',
-  2: 'blue',
-  3: 'orange',
-  4: 'red',
-};
+const stagesColors = ['green', 'blue', 'orange', 'red'];
 
 class PuzzleList extends Component {
   render() {
     return (
       <Grid>
+        { this._actions() }
         { this._puzzles() }
       </Grid>
     );
+  }
+
+  _actions() {
+    return (
+      <Grid.Row columns='1'>
+        <Grid.Column>
+          <Button basic size='small' floated='right' content='New Puzzle' onClick={ () => this._createPuzzle() }/>
+        </Grid.Column>
+      </Grid.Row>
+    )
+  }
+
+  _createPuzzle() {
+    Meteor.call('admin.puzzle.create', (error, result) => {
+      if (error) return alert(error.reason);
+      console.log('Created Puzzle', result);
+    });
   }
 
   _puzzles() {
@@ -30,32 +43,34 @@ class PuzzleList extends Component {
     return (
       <Grid.Row columns='1' key={ puzzle._id }>
         <Grid.Column>
-          { puzzle.name } &nbsp; <Label color={ stageColorMap[puzzle.stage] } content={ puzzle.stage }/>
-          <Button basic float='right'
+          <Label color={ stagesColors[puzzle.stage] } content={ puzzle.stage }/>&nbsp; { puzzle.name }
+          <Button basic floated='right'
             content='Edit'
-            onClick={ () => this._edit(puzzle._id) }
+            onClick={ () => this._edit(puzzle) }
           />
-          <Button basic float='right'
+          <Button basic floated='right'
             icon='trash'
             color='red'
-            onClick={ () => this._delete(puzzle._id) }
+            onClick={ () => this._delete(puzzle) }
           />
         </Grid.Column>
       </Grid.Row>
     );
   }
 
-  _edit(puzzleId) {
-    this.props.onEdit(puzzleId);
+  _edit(puzzle) {
+    this.props.onEdit(puzzle);
   }
 
-  _delete(puzzleId) {
-    this.props.onDelete(puzzleId);
+  _delete(puzzle) {
+    this.props.onDelete(puzzle);
   }
 }
 
 PuzzleList.propTypes = {
   puzzles: PropTypes.array.isRequired,
+  onEdit: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default puzzleSubscriber(PuzzleList);
--- client/components/admin/puzzles/puzzle-dashboard.jsx
+++ client/components/admin/puzzles/puzzle-dashboard.jsx
@@ -2,8 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import { Container, Message, Image, Button } from 'semantic-ui-react';
 
-import Dropzone from 'react-dropzone';
-import ImageList from './imports/image-list';
+import PuzzleEditor from './imports/puzzle-editor';
 import PuzzleList from './imports/puzzle-list';
 
 PuzzleDashboard = class PuzzleDashboard extends Component {
@@ -11,17 +10,24 @@ PuzzleDashboard = class PuzzleDashboard extends Component {
     super(props);
 
     this.state = {
+      activePuzzle: null,
       file: null,
       uploaded: null,
     };
   }
 
   render() {
+    const { activePuzzle } = this.state;
     return (
       <Container>
         <PuzzlePageTitle title='Puzzles'/>
 
-        <PuzzleList />
+        { this._editor() }
+        <br/>
+        <PuzzleList
+          onEdit={ (puzzle) => this._editPuzzle(puzzle) }
+          onDelete={ (puzzle) => this._deletePuzzle(puzzle) }
+        />
         {/* <Dropzone
           ref='dropzone'
           multiple={ false }
@@ -44,24 +50,47 @@ PuzzleDashboard = class PuzzleDashboard extends Component {
     );
   }
 
-  onDrop(files) {
-    const file = files[0] || null;
-    if (!file) return;
-    this.setState({ file });
+  _editor() {
+    const { activePuzzle } = this.state;
+    if (activePuzzle) {
+      return <PuzzleEditor
+        puzzle={ activePuzzle }
+        afterUpdate={ () => this.setState({ activePuzzle: null }) }
+      />;
+    }
+    return <Message info content='Select a puzzle to edit...'/>;
   }
 
-  upload() {
-    if (!this.state.file) return;
+  _editPuzzle(puzzle) {
+    this.setState({ activePuzzle: puzzle });
+  }
 
-    Images.insert(this.state.file, (error, fileObj) => {
+  _deletePuzzle(puzzle) {
+    if (!confirm(`Are you sure you want to delete ${puzzle.name} (${puzzle._id})?`)) return;
+    Meteor.call('admin.puzzle.delete', puzzle._id, (error, result) => {
       if (error) return alert(error.reason);
-
-      console.log(fileObj);
-
-      this.setState({
-        file: null,
-        uploaded: fileObj,
-      });
+      console.log(`Deleted puzzle ${puzzle.name} (${puzzle._id})`);
     });
   }
+  //
+  // onDrop(files) {
+  //   const file = files[0] || null;
+  //   if (!file) return;
+  //   this.setState({ file });
+  // }
+  //
+  // upload() {
+  //   if (!this.state.file) return;
+  //
+  //   Images.insert(this.state.file, (error, fileObj) => {
+  //     if (error) return alert(error.reason);
+  //
+  //     console.log(fileObj);
+  //
+  //     this.setState({
+  //       file: null,
+  //       uploaded: fileObj,
+  //     });
+  //   });
+  // }
 }
