5efcebd05836c5e230426183874603c20c753a02
Kyle Rader <kyle@kylerader.ninja>
2017-03-28 08:04:57 -0700

    Implement begin game button

--- client/components/game/imports/game-ui.jsx
+++ client/components/game/imports/game-ui.jsx
@@ -6,10 +6,18 @@ class GameUI extends Component {
   render() {
     const { team } = this.props;
     if (!team.hasBegun) {
-      return <Button fluid size='big' color='blue' content='Click to Begin'/>
+      return <Button fluid size='big' color='blue' content='Click to Begin' onClick={() => this._begin() }/>
+    } else if (!team.puzzles) {
+      return <Loading/>
     } else {
       return this._main();
     }
+  }
+
+  _begin() {
+    Meteor.call('team.begin', (error, result) => {
+      if (error) alert(`Oops! ${error.reason}`);
+    });
   }
 }
 
