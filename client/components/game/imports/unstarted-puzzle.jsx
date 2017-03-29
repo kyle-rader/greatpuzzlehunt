e13f1bb7165c7af59162847d49e19bfc5a05a208
Kyle Rader <kyle@kylerader.ninja>
2017-03-28 17:50:27 -0700

    make modular puzzle qr component

--- client/components/game/imports/unstarted-puzzle.jsx
+++ client/components/game/imports/unstarted-puzzle.jsx
@@ -1,6 +1,7 @@
 import React, {PropTypes} from 'react';
 import { Segment, Header, Button } from 'semantic-ui-react';
-import QR from 'qrcode.react';
+
+import PuzzleQRCode from './puzzle-qr-code';
 
 export default class UnstartedPuzzle extends React.Component {
   constructor(props) {
@@ -14,31 +15,15 @@ export default class UnstartedPuzzle extends React.Component {
     const { team, puzzle, disabled } = this.props;
     const startUrl = `${window.location.origin}/volunteer/time/${team._id}/${puzzle.puzzleId}`;
     return (
-      <Segment disabled={ this.props.disabled }>
+      <Segment disabled={ disabled }>
         <Header as='h3' content={ this.props.puzzle.name }/>
-        { disabled ? null : this._toggleQRButton() }
-        { disabled ? null : this._qrCode(startUrl) }
-      </Segment>
-    );
-  }
-
-  _toggleQRButton() {
-    return <Button basic color='green'
-      content={ this.state.showQRcode ? 'Hide QR Code' : 'Start Puzzle' }
-      onClick={ () => this._toggleQRcode() }
-    />;
-  }
-
-  _toggleQRcode() {
-    this.setState({ showQRcode: !this.state.showQRcode });
-  }
-
-  _qrCode(url) {
-    if (!this.state.showQRcode) return null;
-    return (
-      <Segment basic textAlign='center'>
-        <Header as='h4' content='Show this QR Code to a volunteer to start!'/>
-        <QR value={ url } size={ 212 }/>
+        <PuzzleQRCode
+          team={ team }
+          puzzle={ puzzle }
+          disabled={ disabled }
+          qrLabel='Show QR Code to a Volunteer to Start!'
+          qrButtonLabel='Start Puzzle!'
+        />
       </Segment>
     );
   }
