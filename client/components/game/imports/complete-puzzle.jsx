aad7732a903cccfdb51acb5103569bad977151f1
Kyle Rader <kyle@kylerader.ninja>
2017-03-29 19:05:40 -0700

    Add game complete puzzle component and fix bug for finished puzzles in puzzle progress comp

---
+++ client/components/game/imports/complete-puzzle.jsx
@@ -0,0 +1,27 @@
+import React, {PropTypes} from 'react';
+import { Segment, Header, Message } from 'semantic-ui-react';
+
+import PuzzleProgress from './puzzle-progress';
+
+export default class CompletePuzzle extends React.Component {
+  render() {
+    const { team, puzzle, disabled } = this.props;
+
+    return (
+      <Segment disabled={ disabled }>
+        <Header as='h3' content={ this.props.puzzle.name }/>
+        <PuzzleProgress puzzle={ puzzle }/>
+        <Message positive>
+          <Message.Header>Solved</Message.Header>
+          <pre>{ puzzle.answer }</pre>
+        </Message>
+      </Segment>
+    );
+  }
+}
+
+CompletePuzzle.propTypes = {
+  team: PropTypes.object.isRequired,
+  puzzle: PropTypes.object.isRequired,
+  disabled: PropTypes.bool.isRequired,
+};
