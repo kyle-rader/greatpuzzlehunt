971b4474ea2924a12f74e343f8f3b1a837ef3f4d
Kyle Rader <kyle@kylerader.ninja>
2018-01-07 14:15:25 -0800

    Add processTransaction to create ticket codes

--- server/webhooks/tickets.js
+++ server/webhooks/tickets.js
@@ -2,11 +2,11 @@ import { Meteor } from 'meteor/meteor';
 import { map, extend } from 'lodash';
 
 import { PostRoute } from '../imports/post-route.js';
-import { createTickets } from './imports/create-tickets.js';
+import processTransaction from '../imports/processTransaction.js';
 
 PostRoute.route('/api/tickets', function(params, req, res, next) {
 
-  // Validate access token
+  // Validate api token
   if (!params.query.token || params.query.token !== Meteor.settings.accounts.apiKey) {
     Meteor.logger.info(`Request on "/api/tickets" with BAD TOKEN: "${params.query.token}" from ${Meteor.logger.jstring(req.headers)}`);
     res.setHeader('Content-Type', 'application/json');
@@ -20,23 +20,5 @@ PostRoute.route('/api/tickets', function(params, req, res, next) {
   res.end();
 
   Meteor.logger.info(`Request on "/api/tickets" from ${Meteor.logger.jstring(req.headers)}`);
-  Meteor.logger.logobj(req.body);
-  Meteor.logger.logobj(JSON.parse(req.body.gear));
-
-  //
-  // // Store T-Shirt Orders:
-  // const tshirts = map(transaction.tshirts, (tshirtOrder) => {
-  //   return Tshirts.insert(extend(tshirtOrder, {
-  //     email: transaction.primaryContact.email,
-  //     name: transaction.primaryContact.name,
-  //     transactionId: transaction._id,
-  //   }));
-  // });
-  //
-  // // Create new users
-  // const usersIds = map(transaction.participants, (user) => registerUser(user, transaction));
-  // Transactions.update({ _id: transaction._id }, {
-  //   $set: { usersIds },
-  // });
-
+  processTransaction(req.body);
 });
