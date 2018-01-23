af9cd0335514ee1b91465c2e1b3eac1a4ebbf3ef
Kyle Rader <kyle@kylerader.ninja>
2018-01-22 20:57:05 -0800

    Use GridFS - Dropbox is broken

--- server/collections/images.js
+++ server/collections/images.js
@@ -1,10 +1,12 @@
 import { Meteor } from 'meteor/meteor';
-import { isAdmin } from '../imports/method-helpers';
+import { isAdmin } from '../../lib/imports/method-helpers';
 
-const store = new FS.Store.FileSystem("images", { path: "~/file_uploads" });
+const store = new FS.Store.GridFS('images');
+
 Images = new FS.Collection("images", {
   stores: [store],
 });
+
 
 function onImageUploaded(storeName, fileObj) {
   // Check for referenced Sposprs.
@@ -18,7 +20,10 @@ function onImageUploaded(storeName, fileObj) {
 
 // When store has uploaded something check for references that need updating.
 store.on('stored', Meteor.bindEnvironment(onImageUploaded, function(error) {
-  Meteor.logger.error("Error in bindEnvironment", error);
+  if (error) {
+    Meteor.logger.error("Error in bindEnvironment", error.reason);
+    Meteor.logger.logobj(error);
+  }
 }));
 
 Images.allow({
