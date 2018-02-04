7bcc0b9b8327a97a562c49dd7b7e678404114151
Kyle Rader <kyle@kylerader.ninja>
2018-02-04 14:49:42 -0800

    Fix indenting

--- client/components/public/imports/HomePeople.jsx
+++ client/components/public/imports/HomePeople.jsx
@@ -5,23 +5,24 @@ export default class HomePeople extends Component {
   render() {
     return (
       <section className="pattern-bg" id="HomePeople">
+        <Grid  style={{ padding: '4em 0em 0 0', margin:'0'}}>
 
-          <Grid  style={{ padding: '4em 0em 0 0', margin:'0'}}>
-           <Grid.Row centered textAlign='left' verticalAlign="middle">
-             <Grid.Column width={16} >
-               <Header size="huge">Who Are we?</Header>
-               <Container>We are Mind Mobilizers who love our community and the amazing diversity of talents it has to offer :)</Container>
-             </Grid.Column>
-           </Grid.Row>
-           <Grid.Row  centered >
-             <Grid.Column width={16}>
+          <Grid.Row centered textAlign='left' verticalAlign="middle">
+            <Grid.Column width={16} >
+              <Header size="huge">Who Are we?</Header>
+              <Container>We are Mind Mobilizers who love our community and the amazing diversity of talents it has to offer :)</Container>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row  centered >
+            <Grid.Column width={16}>
               <ProfileCards/>
-              </Grid.Column>
-           </Grid.Row>
-         </Grid>
-       <Image fluid src="/img/2016/event-photos/gathering-thin.jpg"/>
-    </section>
+            </Grid.Column>
+          </Grid.Row>
 
+        </Grid>
+        <Image fluid src="/img/2016/event-photos/gathering-thin.jpg"/>
+      </section>
     );
   }
 }
