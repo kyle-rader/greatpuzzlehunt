05a3794179dc3af8ddd16f621023b95dd0347dd2
Kyle Rader <kyle@kylerader.ninja>
2018-02-04 21:57:46 -0800

    Use DebounceSearch in AdminUsers comp

--- client/components/admin/Users/AdminUsers.jsx
+++ client/components/admin/Users/AdminUsers.jsx
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import { Grid, Container, Input, Menu, Icon, Label } from 'semantic-ui-react';
 
+import DebounceSearch from '../../imports/DebounceSearch';
 import AdminUserListTracker from './imports/AdminUserListTracker';
 
 AdminUsers = class AdminUsers extends Component {
@@ -9,43 +10,39 @@ AdminUsers = class AdminUsers extends Component {
     super(props);
     this.state = {
       search: '',
-      searchToPass: '',
     };
-
-    this._updateSearchToPass = _.debounce(() => {
-      this.setState({ searchToPass: this.state.search });
-    }, 500);
   }
 
   render() {
+    const { search } = this.state;
+
     return (
       <Container>
         <PuzzlePageTitle title='Admin: Users'/>
-        { this._renderGrid() }
+
+        <Grid stackable>
+
+          <Grid.Row>
+            <Grid.Column>
+              <DebounceSearch
+                fluid
+                icon='search'
+                placeholder='Search...'
+                delay={350}
+                onSearch={(search) => this.setState({ search })}
+              />
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column>
+              <AdminUserListTracker search={search} />
+            </Grid.Column>
+          </Grid.Row>
+
+        </Grid>
+
       </Container>
     );
-  }
-
-  _renderGrid() {
-    return (
-      <Grid stackable>
-        <Grid.Row>
-          <Grid.Column>
-            <Input name='search' placeholder='Search...' fluid icon='search' value={this.state.search} onChange={(e) => this._handleSearchChange(e)}/>
-          </Grid.Column>
-        </Grid.Row>
-
-        <Grid.Row>
-          <Grid.Column>
-            <AdminUserListTracker search={this.state.searchToPass} />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    );
-  }
-
-  _handleSearchChange(e) {
-    this.setState({ search: e.target.value })
-    this._updateSearchToPass();
   }
 }
