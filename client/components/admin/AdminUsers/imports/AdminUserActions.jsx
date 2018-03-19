4802f2a39ffcfdc6ed121232061cf266b228f5ce
Kyle Rader <kyle@kylerader.ninja>
2018-03-18 23:04:31 -0700

    Implement admin user edit actions

---
+++ client/components/admin/AdminUsers/imports/AdminUserActions.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Button, Icon } from 'semantic-ui-react';
+
+class AdminUserActions extends Component {
+  render() {
+    const {
+      user,
+      onEdit,
+      onPasswordReset,
+      onEmailResend,
+      onDelete,
+      onToggleAdmin,
+      onToggleVolunteer,
+      onVerifyEmail,
+    } = this.props;
+    return (
+      <div>
+        <Button basic
+          color={ user.hasRole('admin') ? 'violet' : null }
+          onClick={ onToggleAdmin }
+          icon={ <Icon name='power'/> }
+        />
+        <Button basic
+          color={ user.hasRole('volunteer') ? 'teal' : null }
+          onClick={ onToggleVolunteer }
+          icon={ <Icon name='heart'/> }
+        />
+        <Button basic
+          onClick={ onVerifyEmail }
+          icon="check"
+          content="Verify Email"
+        />
+        <Button basic
+          onClick={onPasswordReset}
+          icon="lock"
+          color='blue'
+          content="Password Reset"
+        />
+        <Button basic
+          onClick={ onEmailResend }
+          icon="mail"
+          color='violet'
+          content="Send Email Verification"
+        />
+        <Button basic
+          onClick={ onDelete }
+          icon='trash'
+          color='red'
+        />
+      </div>
+    );
+  }
+
+  _editIcon() {
+    return this.props.editMode ? 'close' : 'pencil';
+  }
+
+  _editColor() {
+    return this.props.editMode ? 'red' : 'green';
+  }
+}
+
+AdminUserActions.propTypes = {
+  user: PropTypes.object.isRequired,
+  onPasswordReset: PropTypes.func.isRequired,
+  onEmailResend: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onToggleAdmin: PropTypes.func.isRequired,
+  onToggleVolunteer: PropTypes.func.isRequired,
+  onVerifyEmail: PropTypes.func.isRequired,
+};
+
+export default AdminUserActions;
--- client/components/admin/AdminUsers/imports/AdminUserModal.jsx
+++ client/components/admin/AdminUsers/imports/AdminUserModal.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { Modal } from 'semantic-ui-react';
 
 import AdminUserEditForm from './AdminUserEditForm';
+import AdminUserActions from './AdminUserActions';
 
 class AdminUserModal extends Component {
   constructor(props) {
@@ -34,9 +35,97 @@ class AdminUserModal extends Component {
           <AdminUserEditForm user={user}/>
         </Modal.Content>
         <Modal.Actions>
+          <AdminUserActions
+            user={user}
+            onPasswordReset={(e) => this._sendPasswordReset(e)}
+            onEmailResend={(e) => this._resendEmailVerification(e)}
+            onDelete={(e) => this._deleteUser(e)}
+            onToggleAdmin={(e) => this._toggleRole('admin')}
+            onToggleVolunteer={(e) => this._toggleRole('volunteer')}
+            onVerifyEmail={(e) => this._verifyEmail(e)}
+          />
         </Modal.Actions>
       </Modal>
     );
+  }
+
+  _sendPasswordReset(event) {
+    if (!confirm(`Confirm Send Password Reset for "${this.props.user.name}" ?`))
+      return;
+
+    const btn = $(event.target);
+
+    Meteor.call('admin.user.resetPassword', { _id: this.props.user._id }, (err, result) => {
+      if (err) {
+        console.log(err);
+        btn.attr('data-content', 'Failed to send password reset email! 😰');
+      } else {
+        btn.attr('data-content', 'Password Reset Email Sent! 😀');
+      }
+
+      btn.popup({
+        on: 'manual'
+      }).popup('show');
+
+      Meteor.setTimeout(() => {
+        btn.popup('hide');
+      }, 2500);
+    });
+  }
+
+  _resendEmailVerification(event) {
+    if (!confirm(`Confirm Resend Verification email for "${this.props.user.name}" ?`))
+      return;
+
+    const btn = $(event.target);
+
+    Meteor.call('admin.user.emailResend', this.props.user._id, (err, result) => {
+      if (err) {
+        console.log(err);
+        btn.attr('data-content', `Send Failed! 😰 ${err.reason}`);
+      } else {
+        btn.attr('data-content', 'Email Sent! 😀');
+      }
+
+      btn.popup({
+        on: 'manual'
+      }).popup('show');
+
+      Meteor.setTimeout(() => {
+        btn.popup('hide');
+      }, 2500);
+    });
+  }
+
+  _deleteUser(event) {
+    if (!confirm(`Confirm DELETE "${this.props.user.name}" !?!?`))
+      return;
+
+    let btn = $(event.target);
+
+    Meteor.call('admin.user.delete', this.props.user._id, (err, result) => {
+      if (err) {
+        alert(err);
+        btn.attr('data-content', 'Failed to delete user! 😰');
+      }
+    });
+  }
+
+  _toggleRole(role) {
+    const { user } = this.props;
+    if (!confirm(`Toggle ${role} for ${user.name} ?`)) return;
+
+    Meteor.call('admin.user.toggleRole', user._id, role, (err, result) => {
+      if (err) return alert(err);
+    });
+  }
+
+
+  _verifyEmail(e) {
+    e.preventDefault();
+    Meteor.call('admin.validateUser', this.props.user._id, (error, result) => {
+      if (error) alert(error.reason);
+    });
   }
 }
 
