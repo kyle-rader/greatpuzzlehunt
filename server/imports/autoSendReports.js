055dcdca8435238be46807270697ded5b52958ba
Kyle Rader <kyle@kylerader.ninja>
2018-04-07 18:17:37 -0700

    Skip admin reports if send list is empty

--- server/imports/autoSendReports.js
+++ server/imports/autoSendReports.js
@@ -15,6 +15,11 @@ function autoSendReports() {
 
   Meteor.logger.info(`Checking to send auto reports. now: ${now} .. waiting for ${waitingUntil}`);
 
+  if (gameState.sendReportsTo.length <= 0) {
+    Meteor.logger.info("Skipping admin reports because \"gameState.sendReportsTo\" is empty!");
+    return;
+  }
+
   if (now.isAfter(waitingUntil)) {
     const sendTime = moment(now).startOf('hour');
     Meteor.logger.info(`Sending auto reports at ${sendTime} to: ${gameState.sendReportsTo}`);
