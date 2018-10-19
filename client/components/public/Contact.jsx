e328c74f805044817162b456570410b5078cd810
Noah <noah@inspiredware.com>
2018-10-18 22:38:35 -0700

    Update members/leadership; other adjustments to contact page

--- client/components/public/Contact.jsx
+++ client/components/public/Contact.jsx
@@ -10,13 +10,12 @@ Contact = class Contact extends Component {
     <div className="ui container">
       <PuzzlePageTitle title="Contact"/>
       <ICEContact/>
-      <br/>
-      <ProfileCards />
       <h3>
         For Event Questions Contact Millie at <a href='mailto:millie@greatpuzzlehunt.com'>millie.johnson@wwu.edu</a>
         <br/><br/>
         For Account/Tech Questions Contact <a href='mailto:support@greatpuzzlehunt.com'>support@greatpuzzlehunt.com</a>
       </h3>
+      <ProfileCards />
     </div>
     );
   }
--- client/components/public/imports/ProfileCards.jsx
+++ client/components/public/imports/ProfileCards.jsx
@@ -7,114 +7,123 @@ export default class ProfileCards extends Component {
   constructor(props) {
     super(props);
 
-    this.profiles = [
+    this.currentProfiles = [
       {
         name: 'Millie Johnson',
         title: 'Associate Math Professor, WWU',
         role: 'Primary event coordinator, puzzle creator, and founder of the Great Puzzle Hunt',
-        email: 'Millie.Johnson@wwu.edu',
         image: '/img/2016/event-photos/millie_johnson.jpg',
       },
       {
         name: 'Wendy Aguilar',
         title: 'Web Design Specialist, CDK Global',
         role: 'Lead designer',
-        email: 'agu.wnd@gmail.com',
         image: '/img/team/wendy.jpg',
       },
       {
         name: 'Noah Strong',
         title: '',
         role: 'Lead Developer',
-        email: 'noah@greatpuzzlehunt.com',
         image: '/img/team/noah.jpg',
       },
       /* Second Row */
       {
-        name: 'Zoe & Jeff',
-        title: '',
-        role: 'Puzzle Master Team',
-        email: 'gphpuzzlemasters@gmail.com',
-        image: '/img/team/jeff-zoe.jpg',
-      },
-      {
         name: 'Zac Pontrantolfi',
         title: '',
         role: 'Puzzle Master Team',
-        email: 'Zac.Pontrantolfi@gmail.com',
         image: '/img/team/zac.jpg',
       },
       {
         name: 'Alex Covington',
         title: '',
         role: 'Developer',
-        email: '',
         image: '/img/team/alex_c.jpg',
       },
       {
         name: 'Jordan King',
         title: '',
-        role: 'Co-President',
-        email: '',
+        role: 'AS Club Co-President',
         image: '/img/team/jordan_k.jpg',
       },
       {
         name: 'Elias Peters',
         title: '',
-        role: 'Co-President',
-        email: '',
+        role: 'AS Club Co-President',
         image: '/img/team/elias_p.jpg',
       },
       {
         name: 'Katrina Duttkin',
         title: '',
-        role: 'Marketing and Promotions',
-        email: '',
+        role: 'AS Marketing and Promotions',
         image: '/img/team/katrina.jpg',
       },
       {
         name: 'Danielle Glewwe',
         title: '',
-        role: 'Marketing and Promotions',
-        email: '',
+        role: 'AS Marketing and Promotions',
         image: '/img/team/danielle_g.jpg',
       },
       {
         name: 'Miranda Reed',
         title: '',
-        role: 'Co-Budget Coordinator',
-        email: '',
+        role: 'AS Co-Budget Coordinator',
         image: '/img/team/miranda_r.jpeg',
       },
       {
         name: 'Alex Kuhn',
         title: '',
-        role: 'Co-Budget Coordinator',
-        email: '',
+        role: 'AS Co-Budget Coordinator',
         image: '/img/team/',
       },
     ];
+    this.pastProfiles = [
+      {
+        name: 'Kyle Rader',
+        title: '',
+        role: 'Lead developer and secondary event coordinator',
+        image: '/img/team/kyle.jpg',
+      },
+      {
+        name: 'Zoe & Jeff',
+        title: '',
+        role: 'Puzzle Master Team',
+        image: '/img/team/jeff-zoe.jpg',
+      },
+      {
+        name: 'Scott St. Clair',
+        title: '',
+        role: 'AS Club President 2017-18',
+        image: '/img/team/scott.jpg',
+      },
+    ]
   }
 
   render() {
     return (
-      <Card.Group stackable doubling itemsPerRow={3}>
-        { this._renderProfiles() }
-      </Card.Group>
+      <div>
+        <h1>Our Leadership</h1>
+        <Card.Group stackable doubling itemsPerRow={3}>
+          { this._renderProfiles(this.currentProfiles) }
+        </Card.Group>
+
+        <h1>Past Leadership</h1>
+        <Card.Group stackable doubling itemsPerRow={3}>
+          { this._renderProfiles(this.pastProfiles) }
+        </Card.Group>
+      </div>
     );
   }
 
-  _renderProfiles() {
-    return map(this.profiles, (profile) => this._renderProfileCard(profile));
+  _renderProfiles(profiles) {
+    return map(profiles, (profile) => this._renderProfileCard(profile));
   }
 
-  _renderProfileCard({ name, title, role, email, image }) {
+  _renderProfileCard({ name, title, role, image }) {
     return <Card key={name}
       image={image}
       header={name}
       meta={title}
       description={role}
-      extra={<a href={`mailto:${email}`}>{email}</a>}
     />;
   }
 }
