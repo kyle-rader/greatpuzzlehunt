462746fb484dd8cde03dd8ed6afb1e0085ed74b9
alex <covinga@wwu.edu>
2019-01-21 17:10:59 -0800

    Test for creating a user. Started test for verifying/modifying a user

--- tests/puzzles.test.js
+++ tests/puzzles.test.js
@@ -1,4 +1,60 @@
 const { admin } = require('../settings-development');
+const { testUser } = require('./config');
+
+describe('User management', () => {
+  // it('can register a new user', () => {
+  //   browser.url('http://localhost:3000/register');
+
+  //   // Fill out all fields except phone
+  //   browser.setValue('input[name="firstname"]', testUser.firstName);
+  //   browser.setValue('input[name="lastname"]', testUser.lastName);
+  //   browser.setValue('input[name="email"]', testUser.email);
+  //   browser.click('div[name="accountType"]');
+  //   browser.click('div[name="accountType"] div.menu .item');
+  //   browser.setValue('input[name="password"]', testUser.password);
+  //   browser.setValue('input[name="confirmPassword"]', testUser.password);
+  //   browser.setValue('input[name="age"]', testUser.age);
+  //   browser.setValue('input[name="address"]', testUser.address);
+  //   browser.setValue('input[name="city"]', testUser.city);
+  //   browser.setValue('input[name="zip"]', testUser.zip);
+  //   browser.click('div[name="state"]');
+  //   browser.click('div[name="state"] div.menu .item');
+  //   browser.setValue('input[name="ecName"]', testUser.emergencyContact);
+  //   browser.setValue('input[name="ecRelationship"]', testUser.relationship);
+  //   browser.setValue('input[name="ecPhone"]', testUser.phone);
+  //   browser.setValue('input[name="ecEmail"]', testUser.email);
+
+  //   const holdHarmlessCheckbox = browser.$('input[name="holdHarmless"]').$('..');
+  //   holdHarmlessCheckbox.click();
+
+  //   // Try to submit form
+  //   browser.click('button[type="submit"]');
+
+  //   // Form should have failed submission, make sure the error message popped up
+  //   browser.$('div[title="There were issues registering!"]');
+
+  //   // Fill in the phone number field
+  //   browser.setValue('input[name="phone"]', testUser.phone);
+
+  //   // Submit the form
+  //   browser.click('button[type="submit"]');
+  // });
+  it('can verify and modify a user', () => {
+    // Login as admin
+    browser.url('http://localhost:3000/admin/users');
+    browser.setValue('input[name="email"]', admin.email);
+    browser.setValue('input[name="password"]', admin.password);
+    browser.click('button[type="submit"]');
+    browser.waitUntil(() => {
+      return browser.getUrl() === 'http://localhost:3000/admin/users';
+    });
+
+    const userRow = browser.$('tr td span');
+    console.log(userRow);
+    while(true) {}
+
+  });
+})
 
 describe('Puzzle management', () => {
   beforeEach(() => {
@@ -39,13 +95,14 @@ describe('Puzzle management', () => {
     // Check that puzzle properly created by refreshing page and checking list for puzzle
     browser.url('http://localhost:3000/admin/puzzles');
     let found = false;
-    browser.$$('div.column').forEach((e) => {
+    browser.$$('.column.five.row').forEach((e) => {
       try {
         if (e.getText().indexOf('Test Puzzle') !== -1) found = true;
       } catch (err) {
         // Pass
       }
     });
+    console.log(found);
 
     while(true) {}
   });
